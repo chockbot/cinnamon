62c1fe83cf0dba5558fc06c570b4fc5f1d3c5d7e
Keitri <keitrisolutions@gmail.com>
2022-09-06 23:04:26 +0800

    Added Track for Landing Page and Explore Page

--- src/Cinnamon.Web/JsLib/src/mixpanel_lib.js
+++ src/Cinnamon.Web/JsLib/src/mixpanel_lib.js
@@ -1,2 +1,7 @@
 ﻿import mixpanel from 'mixpanel-browser';
 
+export function track(event) {
+    // This is the Mixpanel Cinnamon DEV Project
+    mixpanel.init('7d44367dd73a14d76f134da20250dcb5', { debug: true });
+    mixpanel.track(event); 
+}
