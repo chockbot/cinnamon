0ed6c89e4eae89269e238dd46633b47702c3b818
Rica Namoro <ricanamoro00@gmail.com>
2022-09-26 11:16:29 +0800

    Mobile View and Activity Filter

--- src/Cinnamon.Web/wwwroot/js/Explore.js
+++ src/Cinnamon.Web/wwwroot/js/Explore.js
@@ -1,20 +1,9 @@
-﻿var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-var cardWidth = $(".carousel-item").width();
-var scrollPosition = 0;
+﻿export function ClickIcon() {
+	$(".star-icon i").click(function () {
+		$(this).toggleClass("fa-star fa-star-o");
+	});
+}
 
-$(".carousel-control-next").on("click", function () {
-    if (scrollPosition < (carouselWidth - cardWidth * 11)) { //check if you can go any further
-        scrollPosition += cardWidth;  //update scroll position
-        $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600); //scroll left
-    }
-});
-
-$(".carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
-        scrollPosition -= cardWidth;
-        $(".carousel-inner").animate(
-            { scrollLeft: scrollPosition },
-            600
-        );
-    }
-});
+export function HideModal() {
+	$("#sigupModal").modal('hide');
+}
