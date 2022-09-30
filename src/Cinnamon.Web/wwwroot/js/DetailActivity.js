71727045fa05685ef7cb11db1427c1a9e547de7d
theone0567 <112147456+theone0567@users.noreply.github.com>
2022-09-30 09:47:21 +0800

    Fixed Schedule Carousel

--- src/Cinnamon.Web/wwwroot/js/DetailActivity.js
+++ src/Cinnamon.Web/wwwroot/js/DetailActivity.js
@@ -1,22 +1,24 @@
-﻿$(document).ready(function () {
-    if (document.getElementById("carouselSchedule") != null) {
-        let items = document.getElementById("carouselSchedule").querySelectorAll('.carousel .carousel-item')
+﻿export function OnLoadJavascript() {
+    $(document).ready(function () {
+        if (document.getElementById("carouselSchedule") != null) {
+            let items = document.getElementById("carouselSchedule").querySelectorAll('.carousel .carousel-item')
 
-        items.forEach((el, i) => {
-            if (i == 0) {
-                $(el).addClass("active");
-            }
-            const minPerSlide = 2
-            let next = el.nextElementSibling
-            for (var i = 1; i < minPerSlide; i++) {
-                if (!next) {
-                    // wrap carousel by using first child
-                    next = items[0]
+            items.forEach((el, i) => {
+                if (i == 0) {
+                    el.classList.add("active");
                 }
-                let cloneChild = next.cloneNode(true)
-                el.appendChild(cloneChild.children[0])
-                next = next.nextElementSibling
-            }
-        })
-    }
-});
+                const minPerSlide = 2
+                let next = el.nextElementSibling
+                for (var i = 1; i < minPerSlide; i++) {
+                    if (!next) {
+                        // wrap carousel by using first child
+                        next = items[0]
+                    }
+                    let cloneChild = next.cloneNode(true)
+                    el.appendChild(cloneChild.children[0])
+                    next = next.nextElementSibling
+                }
+            })
+        }
+    });
+}
