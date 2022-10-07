5768f0329a1e4bcbbaf51de51c88d97be53674e3
theone0567 <112147456+theone0567@users.noreply.github.com>
2022-10-07 13:47:21 +0800

    layout update design

--- src/Cinnamon.Web/wwwroot/js/ExperienceCreation.js
+++ src/Cinnamon.Web/wwwroot/js/ExperienceCreation.js
@@ -25,12 +25,17 @@ function showTab(n) {
 
 function fixStepIndicator(n) {
     // This function removes the "active" class of all steps...
-    var i, x = document.getElementsByClassName("icon-step");
+    var i, x = document.getElementsByClassName("icon-step"), step = document.getElementsByClassName("step");
     for (i = 0; i < x.length; i++) {
         x[i].className = x[i].className.replace(" active", "");
     }
+
+    for (i = 0; i < step.length; i++) {
+        step[i].className = step[i].className.replace(" active", "");
+    }
     //... and adds the "active" class to the current step:
     x[n].className += " active";
+    step[n].className += " active";
 }
 
 export function nextPrev(n) {
@@ -76,6 +81,7 @@ function validateForm() {
     // If the valid status is true, mark the step as finished and valid:
     if (valid) {
         document.getElementsByClassName("icon-step")[currentTab].className += " finish";
+        document.getElementsByClassName("step")[currentTab].className += " finish";
     }
     return valid; // return the valid status
 }
