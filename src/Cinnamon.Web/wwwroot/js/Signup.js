715c0b8f8469e011c55baa4eb3caeeedbb45a657
theone0567 <112147456+theone0567@users.noreply.github.com>
2022-10-17 12:04:01 +0800

    addded signupViewModel and Remove some javascript files

--- src/Cinnamon.Web/wwwroot/js/Signup.js
+++ src/Cinnamon.Web/wwwroot/js/Signup.js
@@ -1,17 +1,3 @@
-﻿export function onClickSignup() {
-    $("#signup-waiting").attr("disabled", true);
-    $("#email-signup").attr("disabled", true);
-    $("input[name=signup-value]").attr("disabled", true);
-    $("#spinner-loading").css("visibility", "visible");
-}
-
-export function onDoneSignup() {
-    $("#signup-waiting").attr("disabled", false);
-    $("#email-signup").attr("disabled", false);
-    $("input[name=signup-value]").attr("disabled", false);
-    $("#spinner-loading").css("visibility", "hidden");
-}
-
-export function HideModal() {
+﻿export function HideModal() {
     $("#sigupModal").modal('hide');
 }
