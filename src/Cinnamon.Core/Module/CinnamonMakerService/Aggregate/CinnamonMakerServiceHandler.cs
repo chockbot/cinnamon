cba9e00ccc6a9ace9a0c3f422c7e99c390396e6c
dechalico <dechalico.dev@gmail.com>
2022-11-02 11:25:58 +0800

    temporary remove registerMaker handler

--- src/Cinnamon.Core/Module/CinnamonMakerService/Aggregate/CinnamonMakerServiceHandler.cs
+++ src/Cinnamon.Core/Module/CinnamonMakerService/Aggregate/CinnamonMakerServiceHandler.cs
@@ -4,22 +4,15 @@ namespace Cinnamon.Core.Module.CinnamonMakerService;
 
 public class CinnamonMakerServiceHandler : ICinnamonMakerService 
 {
-    private readonly IRegisterMaker registerMaker;
-    private readonly IConfirmEmailMaker confirmEmailMaker;
     private readonly ISubmitWaitngList submitWaitngList;
     private readonly IVerifyEmail verifyEmail;
 
-    public CinnamonMakerServiceHandler(IRegisterMaker registerMaker, IConfirmEmailMaker confirmEmailMaker,
-        ISubmitWaitngList submitWaitngList, IVerifyEmail verifyEmail)
+    public CinnamonMakerServiceHandler(ISubmitWaitngList submitWaitngList, IVerifyEmail verifyEmail)
     {
-        this.registerMaker = registerMaker;
-        this.confirmEmailMaker = confirmEmailMaker;
         this.submitWaitngList = submitWaitngList;
         this.verifyEmail = verifyEmail;
     }
 
-    public IRegisterMaker RegisterMaker { get { return registerMaker; } }
-    public IConfirmEmailMaker ConfirmEmailMaker {get { return confirmEmailMaker; } }
     public ISubmitWaitngList SubmitWaitngList {get { return submitWaitngList; } }
     public IVerifyEmail VerifyEmail { get { return verifyEmail; } }
 }
