8af3bf150e00247bb2deffb19b28e3714e0df309
theone0567 <112147456+theone0567@users.noreply.github.com>
2022-11-08 15:02:40 +0800

    fix migration error

---
+++ src/Cinnamon.Data/Migrations/20221108035100_added IsPublished column.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Cinnamon.Data.Migrations
+{
+    public partial class addedIsPublishedcolumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "Activities",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "Activities");
+        }
+    }
+}
