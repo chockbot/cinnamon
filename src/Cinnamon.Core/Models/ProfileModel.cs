04a1d721f5b021c40a5a336d718bffc67dffb73c
Rica Namoro <ricanamoro00@gmail.com>
2022-11-17 22:27:28 +0800

    Display selected profile in image container

---
+++ src/Cinnamon.Core/Models/ProfileModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinnamon.Core
+{
+    public class ProfileModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ImageName { get; set; } = "";
+        public long ImageSize { get; set; }
+        public string ImageLocation { get; set; } = "";
+    }
+}
---
+++ src/Cinnamon.Core/ViewModels/UploadProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Cinnamon.Core.DI.Interfaces.Tables;
+using Cinnamon.Core.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Cinnamon.Core
+{
+    public class UploadProfileViewModel
+    {
+        public bool isClickButton { get; set; } = false;
+    }
+}
+   
+
