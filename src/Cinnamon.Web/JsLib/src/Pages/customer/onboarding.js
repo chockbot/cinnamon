89ccac3c693d5a0d5a56327fcce5a51af9f4b6c4
Rica Namoro <ricanamoro00@gmail.com>
2022-11-22 16:54:00 +0800

    Update in Design/Aesthetic Adjustment

--- src/Cinnamon.Web/JsLib/src/Pages/customer/onboarding.js
+++ src/Cinnamon.Web/JsLib/src/Pages/customer/onboarding.js
@@ -102,10 +102,39 @@ onboarding._initFormRegister = () => {
   });
 };
 
+onboarding._initTouchSwipe = () => {
+    $(document).ready(function () {
+        $(".carousel").carousel({
+            interval: false,
+            pause: true
+        });
+
+        $(".carousel .carousel-inner").swipe({
+            swipeLeft: function (event, direction, distance, duration, fingerCount) {
+                this.parent().carousel('next');
+            },
+            swipeRight: function () {
+                this.parent().carousel('prev');
+            },
+            threshold: 0,
+            tap: function (event, target) {
+                window.location = $(this).find('.carousel-item.active a').attr('href');
+            },
+            excludedElements: "label, button, input, select, textarea, .noSwipe"
+        });
+
+        $('.carousel .carousel-inner').on('dragstart', 'a', function () {
+            return false;
+        });
+
+    });
+}
+
 onboarding.init = () => {
   console.log("onboarding page initiated");
   onboarding._initEmailSignup();
   onboarding._initFormRegister();
+  onboarding._initTouchSwipe();
 };
 
 export default onboarding;
