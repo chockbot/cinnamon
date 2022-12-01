f8f2dad70b022e76c7815c89cf685ecfe98915be
Rica Namoro <ricanamoro00@gmail.com>
2022-12-01 21:45:39 +0800

    Add birthday validation

--- src/Cinnamon.Web/JsLib/src/Components/Modal/finishSignUpModal.js
+++ src/Cinnamon.Web/JsLib/src/Components/Modal/finishSignUpModal.js
@@ -20,6 +20,12 @@ finishSignupModal._initFinishForm = () => {
     if (!birthdate) errors.push("Please provide your Birthdate");
     if (!email) errors.push("Please provide your email address");
     if (!password) errors.push("Please provide your password");
+
+     //Validate Birtday
+      var today = new Date();
+      var birthDate = new Date(birthdate);
+      var age = today.getFullYear() - birthDate.getFullYear();
+      if (age < 18) errors.push("Invalid Birthday");
     // prettier-ignore
     if (confirmPassword !== password) errors.push("Password mismatch");
 
