7d69ec22fe03f1d1c911ad5238777f963639f59d
Rica Namoro <ricanamoro00@gmail.com>
2022-12-05 23:05:52 +0800

    Crop image before uploading.

--- src/Cinnamon.Web/wwwroot/js/editprofileModal.js
+++ src/Cinnamon.Web/wwwroot/js/editprofileModal.js
@@ -8,28 +8,35 @@ $jsPhotoUploadInput.on("change", function (e) {
   if (files.length > 0) {
     var photo = files[0];
 
-    var reader = new FileReader();
-    reader.onload = function (event) {
-      var image = $(".js-avatar-preview")[0];
-      image.src = event.target.result;
+      if (photo.size > 10000000) {
 
-      cropper = new Cropper(image, {
-        viewMode: 1,
-        aspectRatio: 1,
-        minContainerWidth: 400,
-        minContainerHeight: 400,
-        minCropBoxWidth: 271,
-        minCropBoxHeight: 271,
-        movable: true,
-        ready: function () {
-          console.log("ready");
-          console.log(cropper.ready);
-        },
-      });
+          alert("Please upload image less than 10MB.");
+      }
+      else
+      {
+          var reader = new FileReader();
+          reader.onload = function (event) {
+              var image = $(".js-avatar-preview")[0];
+              image.src = event.target.result;
 
-      $(cropperModalId).modal("show");
-    };
-    reader.readAsDataURL(photo);
+              cropper = new Cropper(image, {
+                  viewMode: 1,
+                  aspectRatio: 1,
+                  minContainerWidth: 400,
+                  minContainerHeight: 400,
+                  minCropBoxWidth: 271,
+                  minCropBoxHeight: 271,
+                  movable: true,
+                  ready: function () {
+                      console.log("ready");
+                      console.log(cropper.ready);
+                  },
+              });
+
+              $(cropperModalId).modal("show");
+          };
+          reader.readAsDataURL(photo);
+      }
   }
 });
 
@@ -41,21 +48,42 @@ $(".js-save-cropped-avatar").on("click", function (event) {
   var $button = $(this);
   $button.text("Saving...");
   $button.prop("disabled", true);
+    //Crop
+    const canvas = cropper.getCroppedCanvas(); 
+    //Round
+    const roundedcanvas = getRoundedCanvas(canvas);
+    //Check image Size
+    const size = roundedcanvas.size;
+    //Show
+    const base64encodedImage = roundedcanvas.toDataURL();
 
-  const canvas = cropper.getCroppedCanvas();
-  const base64encodedImage = canvas.toDataURL();
-  imageData = base64encodedImage;
+    imageData = base64encodedImage;
 
-  $("#avatar-crop").attr("src", base64encodedImage);
-  $(cropperModalId).modal("hide");
+    $("#avatar-crop").attr("src", base64encodedImage);
+    $(cropperModalId).modal("hide");
 
-  $button.prop("disabled", false);
-  $button.text("Save");
+    $button.prop("disabled", false);
+    $button.text("Save");
 
-  cropper.destroy();
-  cropper = null;
+    cropper.destroy();
+    cropper = null;
 });
 
+function getRoundedCanvas(sourceCanvas) {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    var width = sourceCanvas.width;
+    var height = sourceCanvas.height;
+    canvas.width = width;
+    canvas.height = height;
+    context.imageSmoothingEnabled = true;
+    context.drawImage(sourceCanvas, 0, 0, width, height);
+    context.globalCompositeOperation = 'destination-in';
+    context.beginPath();
+    context.arc(width / 2, height / 2, Math.min(width, height) / 2, 0, 2 * Math.PI, true);
+    context.fill();
+    return canvas;
+}
 export function ChangeContentJS(dotnetHelper) {
   dotnetHelper.invokeMethodAsync("ChangeParaContentValue", imageData);
 }
@@ -65,3 +93,4 @@ $(".btn-close").on("click", function (event) {
   cropper.destroy();
   cropper = null;
 });
+
