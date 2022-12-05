9db35eca15a9457aeb94c21ebce34ffe36b4dfa2
dechalico <dechalico.dev@gmail.com>
2022-12-05 16:07:56 +0800

    update js interop

--- src/Cinnamon.Web/wwwroot/js/editprofileModal.js
+++ src/Cinnamon.Web/wwwroot/js/editprofileModal.js
@@ -1,69 +1,67 @@
 ﻿let cropper;
-let cropperModalId = '#cropperModal';
-let $jsPhotoUploadInput = $('.js-photo-upload');
+let cropperModalId = "#cropperModal";
+let $jsPhotoUploadInput = $(".js-photo-upload");
 let imageData;
 
+$jsPhotoUploadInput.on("change", function (e) {
+  var files = this.files;
+  if (files.length > 0) {
+    var photo = files[0];
 
-$jsPhotoUploadInput.on('change', function (e) {
-    var files = this.files;
-    if (files.length > 0) {
-        var photo = files[0];
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var image = $(".js-avatar-preview")[0];
+      image.src = event.target.result;
 
-        var reader = new FileReader();
-        reader.onload = function (event) {
-            var image = $('.js-avatar-preview')[0];
-            image.src = event.target.result;
+      cropper = new Cropper(image, {
+        viewMode: 1,
+        aspectRatio: 1,
+        minContainerWidth: 400,
+        minContainerHeight: 400,
+        minCropBoxWidth: 271,
+        minCropBoxHeight: 271,
+        movable: true,
+        ready: function () {
+          console.log("ready");
+          console.log(cropper.ready);
+        },
+      });
 
-            cropper = new Cropper(image, {
-                viewMode: 1,
-                aspectRatio: 1,
-                minContainerWidth: 400,
-                minContainerHeight: 400,
-                minCropBoxWidth: 271,
-                minCropBoxHeight: 271,
-                movable: true,
-                ready: function () {
-                    console.log('ready');
-                    console.log(cropper.ready);
-                }
-            });
-
-            $(cropperModalId).modal('show');
-        };
-        reader.readAsDataURL(photo);
-    }
+      $(cropperModalId).modal("show");
+    };
+    reader.readAsDataURL(photo);
+  }
 });
 
-$('.js-save-cropped-avatar').on('click', function (event) {
-    event.preventDefault();
+$(".js-save-cropped-avatar").on("click", function (event) {
+  event.preventDefault();
 
-    console.log(cropper.ready);
+  console.log(cropper.ready);
 
-    var $button = $(this);
-    $button.text('Saving...');
-    $button.prop('disabled', true);
+  var $button = $(this);
+  $button.text("Saving...");
+  $button.prop("disabled", true);
 
-    const canvas = cropper.getCroppedCanvas();
-    const base64encodedImage = canvas.toDataURL();
-    imageData = base64encodedImage;
+  const canvas = cropper.getCroppedCanvas();
+  const base64encodedImage = canvas.toDataURL();
+  imageData = base64encodedImage;
 
-    $('#avatar-crop').attr('src', base64encodedImage);
-    $(cropperModalId).modal('hide');
+  $("#avatar-crop").attr("src", base64encodedImage);
+  $(cropperModalId).modal("hide");
 
-    $button.prop('disabled', false);
-    $button.text('Save');
+  $button.prop("disabled", false);
+  $button.text("Save");
 
-    cropper.destroy();
-    cropper = null;
+  cropper.destroy();
+  cropper = null;
 });
 
-export function ChangeContentJS() {
-    DotNet.invokeMethodAsync('Cinnamon.Web', "ChangeParaContentValue", imageData);
+export function ChangeContentJS(dotnetHelper) {
+  dotnetHelper.invokeMethodAsync("ChangeParaContentValue", imageData);
 }
 
-$('.btn-close').on('click', function (event) {
-    $(cropperModalId).modal('hide');
-    cropper.destroy();
-    cropper = null;
+$(".btn-close").on("click", function (event) {
+  $(cropperModalId).modal("hide");
+  cropper.destroy();
+  cropper = null;
 });
-
