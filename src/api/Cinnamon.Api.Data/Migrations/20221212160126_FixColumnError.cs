58f3930b57a3e88bd05250706426f9e575147f5e
dechalico <dechalico.dev@gmail.com>
2022-12-13 00:04:23 +0800

    add migrations

---
+++ src/api/Cinnamon.Api.Data/Migrations/20221212160126_FixColumnError.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinnamon.Api.Data.Migrations
+{
+    public partial class FixColumnError : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Subcategory",
+                table: "SubCategory",
+                newName: "SubCatergory");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "SubCategory",
+                newName: "CatergoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SubCatergory",
+                table: "SubCategory",
+                newName: "Subcategory");
+
+            migrationBuilder.RenameColumn(
+                name: "CatergoryId",
+                table: "SubCategory",
+                newName: "CategoryId");
+        }
+    }
+}
