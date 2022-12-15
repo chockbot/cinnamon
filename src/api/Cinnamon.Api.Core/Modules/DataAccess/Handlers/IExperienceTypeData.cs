a1c5d13563c85f2a9f3ccfd0e48c574a0f212004
Rica Namoro <ricanamoro00@gmail.com>
2022-12-16 04:39:56 +0800

    Experience Type

---
+++ src/api/Cinnamon.Api.Core/Modules/DataAccess/Handlers/IExperienceTypeData.cs
@@ -0,0 +1,12 @@
+﻿using Cinnamon.Framework.ApiCommand.ApiData.ExperienceType.Request;
+using Cinnamon.Framework.ApiCommand.ApiData.ExperienceType.Response;
+using Cinnamon.Framework.Common;
+
+namespace Cinnamon.Api.Core.Modules.DataAccess.Handlers;
+public interface IExperienceTypeData
+{
+    Task<AppResult<GetExperienceTypeResult>> GetExperienceTypeById(int id);
+    Task<AppResult<GetAllExperienceTypeResult>> GetAllExperienceType();
+    Task<AppResult<CreateExperienceTypeResult>> CreateExperienceType(string name);
+    Task<AppResult<UpdateExperienceTypeResult>> UpdateExperienceType(UpdateExperienceTypeArgs args);
+}
