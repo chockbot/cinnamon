af7a65d595b89fe84f8303643bcb6a083fd0873a
Rica Namoro <ricanamoro00@gmail.com>
2022-12-15 10:25:38 +0800

    SearchTags Table

---
+++ src/api/Cinnamon.Api.Core/Modules/DataAccess/Handlers/ISearchTagsData.cs
@@ -0,0 +1,13 @@
+﻿using Cinnamon.Framework.ApiCommand.ApiData.SearchTags.Request;
+using Cinnamon.Framework.ApiCommand.ApiData.SearchTags.Response;
+using Cinnamon.Framework.Common;
+
+namespace Cinnamon.Api.Core.Modules.DataAccess.Handlers;
+
+public interface ISearchTagsData
+{
+    Task<AppResult<GetSearchTagsResult>> GetSearchTagsById(int id);
+    Task<AppResult<GetAllSearchTagsResult>> GetAlSearchTags(GetAllSearchTagsArgs args);
+    Task<AppResult<CreateSearchTagsResult>> CreateSearchTags(CreateSearchTagsArgs args);
+    Task<AppResult<UpdateSearchTagsResult>> UpdateSearchTags(UpdateSearchTagsArgs args);
+}
