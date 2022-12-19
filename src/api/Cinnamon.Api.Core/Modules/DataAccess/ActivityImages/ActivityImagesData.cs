357df7f85812e823c0d4f37ad44f04bf3b891f5f
Rica Namoro <ricanamoro00@gmail.com>
2022-12-19 11:26:37 +0800

    Activity Image Api

---
+++ src/api/Cinnamon.Api.Core/Modules/DataAccess/ActivityImages/ActivityImagesData.cs
@@ -0,0 +1,105 @@
+﻿using Cinnamon.Api.Core.Config;
+using Cinnamon.Api.Core.Modules.DataAccess.Handlers;
+using Cinnamon.Framework.ApiCommand.ApiData.ActivityImage.Request;
+using Cinnamon.Framework.ApiCommand.ApiData.ActivityImage.Response;
+using Cinnamon.Framework.ApiCommand.ApiData.ExperienceCategory.Response;
+using Cinnamon.Framework.Common;
+using Flurl.Http;
+using Flurl.Http.Configuration;
+
+namespace Cinnamon.Api.Core.Modules.DataAccess.ActivityImages;
+
+public class ActivityImagesData:IActivityImages
+{
+    private readonly IFlurlClient flurlClient;
+	public ActivityImagesData(ApplicationConfig config, IFlurlClientFactory flurlFac)
+	{
+        flurlClient = flurlFac.Get(config.ApiDataUrl);
+    }
+
+    public async Task<AppResult<CreateActivityImageResult>> CreateActivityImage(CreateActivityImageArgs args)
+    {
+        try
+        {
+            var result = await flurlClient
+                .Request("ActivityImage/CreateActivityImage")
+                .PostJsonAsync(args)
+                .ReceiveJson<CreateActivityImageResult>();
+
+            return AppResult<CreateActivityImageResult>.CreateSucceeded(result, "Successfully posting create activity image api");
+        }
+        catch (FlurlHttpException ex)
+        {
+            return AppResult<CreateActivityImageResult>.CreateFailed(ex, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return AppResult<CreateActivityImageResult>.CreateFailed(ex, "An error occured when posting create activity image api");
+        }
+    }
+
+    public async Task<AppResult<GetActivityImageResult>> GetActivityImageById(int id)
+    {
+        try
+        {
+            var result = await flurlClient
+                            .Request($"ActivityImage/GetActivityImageById/{id}")
+                            .GetJsonAsync<GetActivityImageResult>();
+
+            return AppResult<GetActivityImageResult>.CreateSucceeded(result, "Successfully getting activity image by id api");
+        }
+        catch (FlurlHttpException ex)
+        {
+            return AppResult<GetActivityImageResult>.CreateFailed(ex, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return AppResult<GetActivityImageResult>.CreateFailed(ex, "An error occured when getting activity image by id api");
+        }
+    }
+    public async Task<AppResult<GetAllActivityImagesResult>> GetAllActivityImages(GetAllActivityImagesArgs args)
+    {
+        try
+        {
+            var result = await flurlClient
+                            .Request("ActivityImages/GetAllActivityImages")
+                            .SetQueryParams(
+                                new
+                                {
+                                    countPerPage = args.CountPerPage,
+                                    pageIndex = args.PageIndex
+                                }).GetJsonAsync<GetAllActivityImagesResult>();
+
+            return AppResult<GetAllActivityImagesResult>.CreateSucceeded(result, "Successfully getting get all activity image api");
+        }
+        catch (FlurlHttpException ex)
+        {
+            return AppResult<GetAllActivityImagesResult>.CreateFailed(ex, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return AppResult<GetAllActivityImagesResult>.CreateFailed(ex, "An error occured when getting all activity image api");
+        }
+    }
+
+    public async Task<AppResult<UpdateActivityImageResult>> UpdateActivityImage(UpdateActivityImageArgs args)
+    {
+        try
+        {
+            var result = await flurlClient
+                            .Request("ActivityImage/UpdateActivityImage")
+                            .PostJsonAsync(args)
+                            .ReceiveJson<UpdateActivityImageResult>();
+
+            return AppResult<UpdateActivityImageResult>.CreateSucceeded(result, "Successfully posting update activity image api");
+        }
+        catch (FlurlHttpException ex)
+        {
+            return AppResult<UpdateActivityImageResult>.CreateFailed(ex, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return AppResult<UpdateActivityImageResult>.CreateFailed(ex, "An error occured when posting update activity image api");
+        }
+    }
+}
---
+++ src/api/Cinnamon.Api.Core/Modules/DataAccess/Handlers/IActivityImages.cs
@@ -0,0 +1,13 @@
+﻿using Cinnamon.Framework.ApiCommand.ApiData.ActivityImage.Request;
+using Cinnamon.Framework.ApiCommand.ApiData.ActivityImage.Response;
+using Cinnamon.Framework.Common;
+
+namespace Cinnamon.Api.Core.Modules.DataAccess.Handlers;
+
+public interface IActivityImages
+{
+    Task<AppResult<GetActivityImageResult>> GetActivityImageById(int id);
+    Task<AppResult<GetAllActivityImagesResult>> GetAllActivityImages(GetAllActivityImagesArgs args);
+    Task<AppResult<CreateActivityImageResult>> CreateActivityImage(CreateActivityImageArgs args);
+    Task<AppResult<UpdateActivityImageResult>> UpdateActivityImage(UpdateActivityImageArgs args);
+}
