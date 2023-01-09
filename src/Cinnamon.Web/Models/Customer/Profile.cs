30b3e4407b3bb60461b0395a86528310ee1b9683
dechalico <dechalico.dev@gmail.com>
2023-01-09 23:35:17 +0800

    profile enhancement

--- src/Cinnamon.Web/Models/Customer/Profile.cs
+++ src/Cinnamon.Web/Models/Customer/Profile.cs
@@ -8,5 +8,6 @@ public class Profile
     public CustomerModel Customer { get; set; }
     public CustomerProfile CustomerProfile { get; set; }
     public IList<FamilyMember> FamilyMembers {get; set;}
+    public IList<Activity> OwnedActivities {get; set;}
     public string Token { get; set; }
 }
