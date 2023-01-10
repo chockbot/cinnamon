08559fab67d2bc361c5480773705ba47086bd8ad
Rica Namoro <ricanamoro00@gmail.com>
2023-01-10 11:20:42 +0800

    update

--- src/Cinnamon.Web/JsLib/src/Components/Modal/login.js
+++ src/Cinnamon.Web/JsLib/src/Components/Modal/login.js
@@ -51,7 +51,7 @@ login._initEmailSignup = () => {
 };
 
 login._initFormRegister = () => {
-    $("#form-register-information").submit(async function (e) {
+    k$("#form-register-information").submit(async function (e) {
         e.preventDefault();
 
         const firstname = $("#firstname-signup-register").val();
