7a880bd7877d3892e223437ce6aca96582c0dae4
dechalico <dechalico.dev@gmail.com>
2023-01-12 12:29:51 +0800

    remove old codes

--- src/Cinnamon.Web/Modules/ApiAccess/Handlers/ITransactionApiHandler.cs
+++ src/Cinnamon.Web/Modules/ApiAccess/Handlers/ITransactionApiHandler.cs
@@ -1,6 +1,6 @@
-using Cinnamon.Core.Common;
 using Cinnamon.Framework.ApiCommand.ApiCore.Transaction.Request;
 using Cinnamon.Framework.ApiCommand.ApiCore.Transaction.Response;
+using Cinnamon.Framework.Common;
 
 namespace Cinnamon.Web.Modules.ApiAccess.Handlers;
 
