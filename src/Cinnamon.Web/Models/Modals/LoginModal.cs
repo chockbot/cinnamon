7fd27cbd1430c9e4e75c8d65f648d86d96d31c34
dechalico <dechalico.dev@gmail.com>
2023-01-13 21:25:41 +0800

    update login in explore page

---
+++ src/Cinnamon.Web/Models/Modals/LoginModal.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using Blazorise;
+
+namespace Cinnamon.Web.Models.Modals;
+
+public class LoginModal 
+{
+    public Modal EmailModal {get; set;}
+    public Modal PasswordModal {get; set;}
+    public Modal VerificationModal {get; set;}
+
+    public LoginEmailModel EmailModel {get; set;} = new();
+    public LoginPasswordModel PasswordModel {get; set;} = new();
+
+    public class LoginEmailModel 
+    {
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email {get; set;}
+    }
+
+    public class LoginPasswordModel 
+    {
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(30, ErrorMessage = "Must be between 6 and 30 characters", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password {get; set;}
+    }
+}
