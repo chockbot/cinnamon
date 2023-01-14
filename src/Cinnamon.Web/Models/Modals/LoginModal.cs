66d937681bdbd7624817d347b7c43d340c163f3a
dechalico <dechalico.dev@gmail.com>
2023-01-15 01:56:00 +0800

    login modal

--- src/Cinnamon.Web/Models/Modals/LoginModal.cs
+++ src/Cinnamon.Web/Models/Modals/LoginModal.cs
@@ -8,6 +8,8 @@ public class LoginModal
     public Modal EmailModal {get; set;}
     public Modal PasswordModal {get; set;}
     public Modal VerificationModal {get; set;}
+    public Validations EmailModelValidation {get; set;}
+    public Validations PasswordModelValidation {get; set;}
 
     public LoginEmailModel EmailModel {get; set;} = new();
     public LoginPasswordModel PasswordModel {get; set;} = new();
@@ -18,6 +20,9 @@ public class LoginModal
         [EmailAddress]
         [DataType(DataType.EmailAddress)]
         public string Email {get; set;}
+
+        public bool IsEmailSubmitting {get; set;}
+        public bool IsShowErrorMessage {get; set;}
     }
 
     public class LoginPasswordModel 
@@ -26,5 +31,9 @@ public class LoginModal
         [StringLength(30, ErrorMessage = "Must be between 6 and 30 characters", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password {get; set;}
+
+        public bool IsPasswordSubmitting {get; set;}
+        public bool IShowErrorMessage {get; set;}
+        public string ErrorMessage {get; set;}
     }
 }
