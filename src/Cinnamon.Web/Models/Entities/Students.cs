472fa67dc3dd3dd2047da16145028f1cd94cf345
Rica Namoro <ricanamoro00@gmail.com>
2023-02-01 15:41:05 +0800

    Add Students Entity

---
+++ src/Cinnamon.Web/Models/Entities/Students.cs
@@ -0,0 +1,15 @@
+﻿namespace Cinnamon.Web.Models.Entities;
+
+public class Students
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public int ActivityId { get; set; }
+    public int ScheduleId { get; set; }
+    public string Name { get; set; }
+    public string StudentNo { get; set; }
+    public int NumberOfSessions { get; set; }
+    public int SessionsAttended { get; set; }
+    public string Remarks { get; set; }
+    public string Status { get; set; }
+}
