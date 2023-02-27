3da8d34ea290466a62c02a4b97c73fd11cfc64d0
Mark Daniel Torres <80269@global.ul.com>
2023-02-27 20:51:42 +0800

    changes for CWD-653

--- src/api/Cinnamon.Api.Data/Repository/Entities/ActivityAddress.cs
+++ src/api/Cinnamon.Api.Data/Repository/Entities/ActivityAddress.cs
@@ -7,9 +7,12 @@ public class ActivityAddress : BaseEntity
     public string Address2 {get; set;}
     public string District {get; set;}
     public string City {get; set;}
+    public string CityName {get; set; }
     public string Subdivision { get; set; }
     public string Region { get; set; }
+    public string RegionName { get; set; }
     public string Barangay { get; set; }
+    public string BarangayName { get; set; }
     public string PostalCode { get; set; }
     public virtual Activity Activity {get; set;}
 }
