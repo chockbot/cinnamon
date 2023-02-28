e7d21278737cd07037b0b047635a2a22b6036838
Rica Namoro <ricanamoro00@gmail.com>
2023-02-28 08:35:33 +0800

    Change Delete icon in Experience Setup

---
+++ src/Cinnamon.Web/Models/Dashboard/ManageStudentsModel.cs
@@ -0,0 +1,37 @@
+﻿using Blazorise;
+using Cinnamon.Web.Models.Entities;
+
+namespace Cinnamon.Web.Models.Dashboard;
+
+public class ManageStudentsModel
+{
+    public Models.Customer.Profile Model;
+
+    public List<Activity> activities = new List<Activity>();
+
+    public List<Students> students = new List<Students>();
+
+    public List<ManageStudentsData> manageStudents = new List<ManageStudentsData>();
+
+    public List<ManageStudentsData> FilteredStudents { get; set; } = new();
+
+    public bool? IsSortNameAsc { get; set; }
+
+    public bool? IsSortExperienceAsc { get; set; }
+
+    public string customFilterValue;
+
+    public string selectedFilter;
+
+    public TextEdit textEdit;
+
+    public bool showErrorMessage = false;
+
+    public bool showSuccessMessage = false;
+
+    public string errorMessage = string.Empty;
+
+    public int jerseyNumber;
+
+    public int Id;
+}
