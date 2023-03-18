4a85523ca0a1b60056e9bb659bda4398626c7d5d
Mark Daniel Torres <80269@global.ul.com>
2023-03-18 09:57:37 +0800

    CWD-701 changes

--- src/Cinnamon.Web/JsLib/src/Pages/activity/maxMinWords.js
+++ src/Cinnamon.Web/JsLib/src/Pages/activity/maxMinWords.js
@@ -1,30 +1,27 @@
 const maxMinWords = {};
 
-maxMinWords.max = (selector, max) => {
+maxMinWords.init = (labelSelector, editorSelector, measure, wordsLimit) => {
+    setLabelText(labelSelector, editorSelector, measure, wordsLimit);
+}
+
+maxMinWords.max = (labelSelector, selector, measure, max) => {
   $(selector).keyup(function (e) {
       const inputValue = e.target.value;
       const wordCount = inputValue.trim().split(/\s+/).length;
 
-      if (wordCount >= 15 && !isSpace(inputValue)) {
+      if (wordCount >= max && !isSpace(inputValue)) {
           e.preventDefault();
           e.target.value = truncateText(inputValue);
       }
+
+      setLabelText(labelSelector, selector, measure, max);
   });
 };
 
-maxMinWords.min = (selector, min) => {
-  $(selector).keydown(function (e) {
-    const wordsLength = $(this)
-      .val()
-      .trim()
-      .split(" ")
-      .filter((t) => !!t).length;
-
-    if (wordsLength < min) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-  });
+maxMinWords.min = (labelSelector, selector, measure, min) => {
+    $(selector).keyup(function (e) {
+        setLabelText(labelSelector, selector, measure, min);
+    });
 };
 
 function isSpace(text) {
@@ -33,5 +30,14 @@ function isSpace(text) {
 
 function truncateText(text) {
     return text.trim().split(/\s+/).slice(0, 15).join(' ');
+}
+
+function getWordsLength(editorSelector) {
+    return $(editorSelector).val() ? $(editorSelector).val().trim().split(/\s+/).length : 0;
+}
+
+function setLabelText(labelSelector, editorSelector, measure, count) {
+    $(labelSelector).text(`${measure} of ${count} words (${getWordsLength(editorSelector)}/${count})`);
+
 }
 export default maxMinWords;
