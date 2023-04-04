0298c31bd7a711378a092e91a8d192e5354008e5
Mark Daniel Torres <80269@global.ul.com>
2023-04-04 15:23:34 +0800

    bug-106 changes

---
+++ src/Cinnamon.Framework/ApiCommand/ApiCore/Activity/Response/UpdateScheduleResult.cs
@@ -0,0 +1,8 @@
+using Cinnamon.Framework.ApiCommand.ApiCore.DTO.Activity;
+using Cinnamon.Framework.ApiCommand.ApiData.DTO.Schedule;
+
+namespace Cinnamon.Framework.ApiCommand.ApiCore.Activity.Response;
+
+public class UpdateScheduleResult : AbstractModel<ScheduleDTO>
+{
+}
