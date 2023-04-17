e8500319a2c64c3135012018cc0b0a4637071c86
Rica Namoro <ricanamoro00@gmail.com>
2023-04-17 20:12:20 +0800

    Add 1 when odd number

--- src/api/Cinnamon.Api.Core/Services/OnGoingActivityService/CreateOngoingActivityHandler.cs
+++ src/api/Cinnamon.Api.Core/Services/OnGoingActivityService/CreateOngoingActivityHandler.cs
@@ -98,7 +98,7 @@ public class CreateOngoingActivityHandler : ICreateOngoingActivityHandler
             else
             {
                 decimal value = schedule.PerUnit2 / 2;
-                numberOfBackTracking = Math.Ceiling(value);
+                numberOfBackTracking = Math.Ceiling(value) + 1;
             }
             // enroll the students
             var createStudentRes = await studentData.CreateManyStudent(new Framework.ApiCommand.ApiData.Student.Request.CreateManyStudentArgs {
