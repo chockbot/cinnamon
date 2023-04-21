56de98db54791ee6b0a4aa3c345fd06a8fb201ae
dechalico <dechalico.dev@gmail.com>
2023-04-21 14:53:43 +0800

    update payment confirmation page

--- src/Cinnamon.Web/Extensions/AppExtendServices.cs
+++ src/Cinnamon.Web/Extensions/AppExtendServices.cs
@@ -3,7 +3,7 @@ namespace Cinnamon.Web.Extensions;
 public static class ExtensionService
 {
     public static IServiceCollection AppExtendServices(this IServiceCollection services)
-    {
+    {   
         services.AddTransient<Modules.ApiAccess.Handlers.IAccountApiHandler, Modules.ApiAccess.Account.AccountApiHandler>();
         services.AddTransient<Modules.ApiAccess.Handlers.IActivityApiHandler, Modules.ApiAccess.Activity.ActivityApiHandler>();
         services.AddTransient<Modules.ApiAccess.Handlers.ITransactionApiHandler, Modules.ApiAccess.Transaction.TransactionApiHandler>();
