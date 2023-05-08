4a2b5a52f13b867daf52dc0157b5954a3181cd53
Rica Namoro <ricanamoro00@gmail.com>
2023-05-09 07:37:27 +0800

    -copy to clip board

---
+++ src/Cinnamon.Web/wwwroot/js/clipboard.js
@@ -0,0 +1,11 @@
+﻿window.copyToClipboard = (text) => {
+    var element = document.getElementById(text);
+    var clipboard = new ClipboardJS(element);
+    clipboard.on('success', function (e) {
+        console.log('Copied to clipboard: ' + e.text);
+        e.clearSelection();
+    });
+    clipboard.on('error', function (e) {
+        console.error('Error copying to clipboard: ' + e.text);
+    });
+}
