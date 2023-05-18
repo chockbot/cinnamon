8c05250c454ce93e2e1f6d1047735b56d191c972
dechalico <dechalico.dev@gmail.com>
2023-05-19 01:15:07 +0800

    image compression enhancement

--- src/Cinnamon.Web/JsLib/src/Components/Modal/uploadGovernmentIds.js
+++ src/Cinnamon.Web/JsLib/src/Components/Modal/uploadGovernmentIds.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Compressor from "compressorjs";
 
 const uploadGovernmentIds = {};
 
 uploadGovernmentIds._initForm = (dotnetObj) => {
   $("#submit-government-ids").submit(async function (e) {
     e.preventDefault();
+
+    debugger;
 
     const front = $("#FrontId").val();
     const back = $("#BackId").val();
@@ -23,7 +26,21 @@ uploadGovernmentIds._initForm = (dotnetObj) => {
 
     if (!back || !front) return;
 
-    const formData = new FormData(this);
+    const formData = new FormData();
+
+    try {
+      const image1 = await uploadGovernmentIds.compressImage(
+        $("#FrontId")[0].files[0]
+      );
+      const image2 = await uploadGovernmentIds.compressImage(
+        $("#BackId")[0].files[0]
+      );
+
+      formData.append("FrontId", image1, image1.name);
+      formData.append("BackId", image2, image2.name);
+    } catch (error) {
+      return;
+    }
 
     // show loading spinner
     dotnetObj.invokeMethodAsync("ShowLoading");
@@ -37,7 +54,7 @@ uploadGovernmentIds._initForm = (dotnetObj) => {
     dotnetObj.invokeMethodAsync("HideLoading");
 
     if (data.success) {
-        location.href = "/verificationprocess";
+      location.href = "/verificationprocess";
     } else {
       dotnetObj.invokeMethodAsync("ShowError", data.message);
     }
@@ -46,6 +63,22 @@ uploadGovernmentIds._initForm = (dotnetObj) => {
 
 uploadGovernmentIds.init = (dotnetObj) => {
   uploadGovernmentIds._initForm(dotnetObj);
+};
+
+uploadGovernmentIds.compressImage = (blob) => {
+  return new Promise((resolve, reject) => {
+    if (!blob) return reject();
+
+    new Compressor(blob, {
+      quality: 0.4,
+      success: (result) => {
+        return resolve(result);
+      },
+      error: (error) => {
+        return reject(error);
+      },
+    });
+  });
 };
 
 export default uploadGovernmentIds;
