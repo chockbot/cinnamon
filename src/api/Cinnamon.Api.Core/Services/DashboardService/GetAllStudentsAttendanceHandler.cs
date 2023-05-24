691538e0b3a4e4ad47bae27d275296642d6e8a57
Rica Namoro <ricanamoro00@gmail.com>
2023-05-24 09:41:31 +0800

    - show date of acquisition for RARE, EPIC and LEGENDARY Cinnamon Makers

---
+++ src/api/Cinnamon.Api.Core/Services/DashboardService/GetAllStudentsAttendanceHandler.cs
@@ -0,0 +1,67 @@
+﻿using Cinnamon.Api.Core.Modules.DataAccess.Handlers;
+using Cinnamon.Api.Core.Modules.DataAccess.Student;
+using Cinnamon.Api.Core.Services.ActivityService.Handlers;
+using Cinnamon.Api.Core.Services.DashboardService.Handlers;
+using Cinnamon.Api.Core.Services.DashboardService.Interactors;
+using Cinnamon.Api.Core.Services.DashboardService.Interactors.Results;
+using Cinnamon.Framework.Common;
+
+namespace Cinnamon.Api.Core.Services.DashboardService;
+public class GetAllStudentsAttendanceHandler: IGetAllStudentsAttendanceHandler
+{
+    private readonly IStudentAttendanceData studentAttendanceData;
+    private readonly IStudentData studentData;
+    public GetAllStudentsAttendanceHandler(IStudentAttendanceData studentAttendanceData,IStudentData studentData)
+    {
+        this.studentAttendanceData = studentAttendanceData;
+        this.studentData = studentData;
+    }
+
+    public AppResult<GetAllStudentsAttendanceResult> Execute(GetAllStudentsAttendanceArgs args)
+    {
+        try
+        {
+            return ExecuteAsync(args).Result;
+        }
+        catch (Exception ex)
+        {
+            return AppResult<GetAllStudentsAttendanceResult>.CreateFailed(ex, "An error occured in GetAllStudentAttendanceHandler");
+        }
+    }
+
+    public async Task<AppResult<GetAllStudentsAttendanceResult>> ExecuteAsync(GetAllStudentsAttendanceArgs args)
+    {
+        try
+        {
+            var attendanceResReLoad = await studentAttendanceData.GetAllStudentAttendance(new Framework.ApiCommand.ApiData.StudentAttendance.Request.GetAllStudentAttendanceArgs
+            {
+                ActivityIds = new int[] { args.ActivityId },
+                IsIncludeStudent = true,
+            });
+            if (!attendanceResReLoad.Succeeded || attendanceResReLoad.Result == null || !attendanceResReLoad.Result.IsSuccess)
+            {
+                return AppResult<GetAllStudentsAttendanceResult>.CreateFailed(new ApplicationException(attendanceResReLoad.Result?.ErrorInfo?.Message), attendanceResReLoad.Message);
+            }
+
+            return AppResult<GetAllStudentsAttendanceResult>.CreateSucceeded(new GetAllStudentsAttendanceResult
+            {
+                StudentAttendaces = attendanceResReLoad.Result.Result.Select(s =>
+                {
+                    return new GetAllStudentsAttendanceResult.StudentAttendace
+                    {
+                        Id = s.Id,
+                        IsPresent = s.IsPresent,
+                        NumberOfSessions = s.Student.NumberOfSessions,
+                        SessionsAttended = s.Student.SessionsAttended,
+                        StudentId = s.StudentId,
+                        AttendanceDate = s.Date,
+                    };
+                })
+            }, "Successfully get student attendance");
+        }
+        catch (Exception ex)
+        {
+            return AppResult<GetAllStudentsAttendanceResult>.CreateFailed(ex, "An error occured in GetAllStudentAttendanceHandler");
+        }
+    }
+}
---
+++ src/api/Cinnamon.Api.Core/Services/DashboardService/Interactors/GetAllStudentsAttendanceArgs.cs
@@ -0,0 +1,8 @@
+﻿using Cinnamon.Framework.Interactor;
+
+namespace Cinnamon.Api.Core.Services.DashboardService.Interactors;
+
+public class GetAllStudentsAttendanceArgs : IInteractor
+{
+    public int ActivityId { get; set; }
+}
