1495db0a7c712f034d398d0ccc03b58eeaefbb5a
dechalico <dechalico.dev@gmail.com>
2023-06-07 18:18:40 +0800

    add customer query for getting purchse order transaction inclusive

---
+++ src/Cinnamon.Framework/ApiCommand/ApiData/DTO/PurchaseOrder/InclusivePurchaseOrderDTO.cs
@@ -0,0 +1,23 @@
+namespace Cinnamon.Framework.ApiCommand.ApiData.DTO.PurchaseOrder;
+
+public class InclusivePurchaseOrderDTO 
+{
+    public int PurchaseOrderId {get; set;}
+    public DateTime PurchaseDate {get; set;}
+    public int UnitCount {get; set;}
+    public decimal UnitPrice {get; set;}
+    public decimal Total {get; set;}
+    public decimal ConvinienceFee {get; set;}
+    public decimal CreditAmount {get; set;}
+    public decimal OverallTotal {get; set;}
+    public int Status {get; set;}
+
+    public ProviderDTO Provider {get; set;} = new();
+
+    public class ProviderDTO 
+    {
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Email {get; set;}
+    }
+}
--- src/api/Cinnamon.Api.Data/Repository/DbSets/PurchaseOrderEntity.cs
+++ src/api/Cinnamon.Api.Data/Repository/DbSets/PurchaseOrderEntity.cs
@@ -1,5 +1,8 @@
-﻿using Cinnamon.Api.Data.Repository.Entities;
+﻿using System.Data;
+using System.Data.Entity.Core.EntityClient;
+using Cinnamon.Api.Data.Repository.Entities;
 using Cinnamon.Api.Data.Repository.Interfaces;
+using Cinnamon.Framework.ApiCommand.ApiData.DTO.PurchaseOrder;
 using Cinnamon.Framework.Common;
 using Microsoft.EntityFrameworkCore;
 
@@ -61,6 +64,120 @@ public class PurchaseOrderEntity : GenericEntity<PurchaseOrder>, IPurchaseOrder
         catch (Exception ex)
         {
             return AppResult<IEnumerable<PurchaseOrder>>.CreateFailed(ex, "An error occured when trying to update purchase orders");
+        }
+    }
+
+    public async Task<AppResult<IEnumerable<InclusivePurchaseOrderDTO>>> GetInclusiveTransaction(string? name, DateTime? purchaseDate, string? email, int? status)
+    {
+        try
+        {
+            string queryFilters = string.Empty;
+
+            if(!string.IsNullOrEmpty(name))
+            {
+                queryFilters += "and (cc.\"FirstName\" like '%'@name'%' or cc.\"LastName\" like '%'@name'%') ";
+            }
+
+            if(purchaseDate.HasValue)
+            {
+                queryFilters += "and cast(po.\"CreatedOn\" as date) = @date ";
+            }
+
+            if(!string.IsNullOrEmpty(email))
+            {
+                queryFilters += "and cc.\"Email\" = @email ";
+            }
+
+            if(status.HasValue)
+            {
+                queryFilters += "and po.\"Status\" = @status ";
+            }
+            
+            string query = "select po.\"Id\" as \"POId\", po.\"CreatedOn\", po.\"UnitCount\", po.\"UnitPrice\", " +
+                                "po.\"UnitCount\" * po.\"UnitPrice\" as \"Total\", po.\"ConvinienceFee\", " +
+                                "po.\"CreditAmount\", (po.\"UnitCount\" * po.\"UnitPrice\") - po.\"ConvinienceFee\" as \"OverallTotal\", " +
+                                "po.\"Status\", cc.\"FirstName\", cc.\"LastName\", cc.\"Email\" " +
+                            "from public.\"PurchaseOrders\" po " +
+                            "join public.\"Activities\" ac " +
+                                "on po.\"ActivityId\" = ac.\"Id\" " +
+                            "join public.\"Customers\" cc " +
+                                "on ac.\"CreatedBy\" = cc.\"Id\" " +
+                            "where po.\"IsInclusivePayment\" = true " + queryFilters;
+
+            IList<InclusivePurchaseOrderDTO> listResult = new List<InclusivePurchaseOrderDTO>();
+
+            using(var command = applicationContext.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = query;
+
+                if(!string.IsNullOrEmpty(name))
+                {
+                    var parameterName = new EntityParameter();
+                    parameterName.ParameterName = "name";
+                    parameterName.Value = name;
+                    command.Parameters.Add(parameterName);
+                }
+
+                if(purchaseDate.HasValue)
+                {
+                    var parameterDate = new EntityParameter();
+                    parameterDate.ParameterName = "date";
+                    parameterDate.Value = purchaseDate?.ToString("YYYY-MM-dd");
+                    command.Parameters.Add(parameterDate);
+                }
+
+                if(!string.IsNullOrEmpty(email))
+                {
+                    var parameterEmail = new EntityParameter();
+                    parameterEmail.ParameterName = "email";
+                    parameterEmail.Value = email;
+                    command.Parameters.Add(parameterEmail);
+                }
+
+                if(status.HasValue) 
+                {
+                    var parameterStatus = new EntityParameter();
+                    parameterStatus.ParameterName = "status";
+                    parameterStatus.Value = status;
+                    command.Parameters.Add(parameterStatus);
+                }
+
+                applicationContext.Database.OpenConnection();
+                
+                using(var dr = await command.ExecuteReaderAsync())
+                {
+                    if(dr.HasRows)
+                    {
+                        var dt = new DataTable();
+                        dt.Load(dr);
+
+                        listResult = dt.AsEnumerable().Select(item => new InclusivePurchaseOrderDTO {
+                            ConvinienceFee = Convert.ToDecimal(item["ConvinienceFee"]),
+                            CreditAmount = Convert.ToDecimal(item["CreditAmount"]),
+                            OverallTotal = Convert.ToDecimal(item["OverallTotal"]),
+                            Provider = new InclusivePurchaseOrderDTO.ProviderDTO {
+                                Email = item["Email"].ToString() ?? string.Empty,
+                                FirstName = item["FirstName"].ToString() ?? string.Empty,
+                                LastName = item["LastName"].ToString() ?? string.Empty
+                            },
+                            PurchaseDate = Convert.ToDateTime(item["CreatedOn"]),
+                            PurchaseOrderId = Convert.ToInt32(item["POId"]),
+                            Status = Convert.ToInt32(item["Status"]),
+                            Total = Convert.ToDecimal(item["Total"]),
+                            UnitCount = Convert.ToInt32(item["UnitCount"]),
+                            UnitPrice = Convert.ToDecimal(item["UnitPrice"])
+                        }).ToList();
+                    }
+                }
+
+            }
+
+            return AppResult<IEnumerable<InclusivePurchaseOrderDTO>>.CreateSucceeded(listResult, "Successfuly get list of transactions");
+        }
+        catch (Exception ex)
+        {
+            return AppResult<IEnumerable<InclusivePurchaseOrderDTO>>.CreateFailed(ex, "An error occured when getting inclusive transactions");
         }
     }
 }
