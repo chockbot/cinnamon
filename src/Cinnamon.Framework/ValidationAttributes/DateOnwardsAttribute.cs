449c95384c613f3b348827f5dd72f6e65363f9ed
dechalico <dechalico.dev@gmail.com>
2023-07-13 17:38:56 +0800

    add date less validation

--- src/Cinnamon.Framework/ValidationAttributes/DateOnwardsAttribute.cs
+++ src/Cinnamon.Framework/ValidationAttributes/DateOnwardsAttribute.cs
@@ -5,13 +5,21 @@ namespace Cinnamon.Framework.ValidationAttributes;
 
 public class DateOnwardsAttribute : ValidationAttribute 
 {
+    public bool Strict {get; set;} = true;
+
     public override bool IsValid(object? value)
     {
         if(value == null) return false;
 
         var now = DateTime.Now;
         var date = Convert.ToDateTime(value);
-        
+
+        if(!Strict)
+        {
+            now = now.Date;
+            date = date.Date;
+        }
+
         return date >= now;
     }
 }
