1ae56d3dc2b041ec2b557f57b0615ed62e329265
dechalico <dechalico.dev@gmail.com>
2023-07-18 00:50:48 +0800

    web app add api core module handlers

--- src/Cinnamon.Web/Modules/ApiAccess/Handlers/IAccountApiHandler.cs
+++ src/Cinnamon.Web/Modules/ApiAccess/Handlers/IAccountApiHandler.cs
@@ -40,4 +40,5 @@ public interface IAccountApiHandler
     Task<AppResult<UpdateProfileDetailsResult>> UpdateCustomerProfile(UpdateProfileDetailsArgs args, string token);
     Task<AppResult<UpdateRequestRefundResult>> UpdateRefundRequest(UpdateRequestRefundArgs args, string token);
     Task<AppResult<SubmitAccountVerifiedResult>> SubmitAccountVerified(string token);
+    Task<AppResult<BlockedAccountResult>> BlockAccount(BlockedAccountArgs args, string token);
 } 
