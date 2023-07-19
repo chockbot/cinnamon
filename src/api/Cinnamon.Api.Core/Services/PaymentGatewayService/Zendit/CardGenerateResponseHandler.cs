396a0880cfbb1d1ed8e73b668308599f3c4c188e
dechalico <dechalico.dev@gmail.com>
2023-07-19 15:18:51 +0800

    add delay

--- src/api/Cinnamon.Api.Core/Services/PaymentGatewayService/Zendit/CardGenerateResponseHandler.cs
+++ src/api/Cinnamon.Api.Core/Services/PaymentGatewayService/Zendit/CardGenerateResponseHandler.cs
@@ -119,7 +119,7 @@ public class CardGenerateResponseHandler : IGenerateResponseHandler, ICardDriver
                 .PostJsonAsync(paymentRequest)
                 .ReceiveJson<RequestPaymentResult>();
 
-            await Task.Delay(2000);
+            await Task.Delay(3000);
 
             /* request payment api
             *  this is the main transaction payment
