ee451bee5f53015ce826926de76a8ae4be256a1d
dechalico <dechalico.dev@gmail.com>
2023-08-30 17:50:36 +0800

    xp createtion ui upload changes

--- src/Cinnamon.Web/JsLib/src/Components/Modal/index.js
+++ src/Cinnamon.Web/JsLib/src/Components/Modal/index.js
@@ -2,7 +2,8 @@ import finishSignUpModal from "./finishSignUpModal";
 import passwordModal from "./PasswordModal";
 import uploadGovenmentId from "./uploadGovernmentIds";
 import uploadProfilePicture from "./uploadProfilePicture";
-import login from "./login"
+import login from "./login";
+import imageCropper from "./imageCropper";
 
 export default {
   finishSignUpModal,
@@ -10,4 +11,5 @@ export default {
   uploadGovenmentId,
   uploadProfilePicture,
   login,
+  imageCropper,
 };
