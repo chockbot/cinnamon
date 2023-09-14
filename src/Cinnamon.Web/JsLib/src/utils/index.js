13ec30d95897a243c64630c7b25e28341aa3a805
Rica Namoro <ricanamoro00@gmail.com>
2023-09-14 08:48:24 +0800

    calendar in enrolle master list

--- src/Cinnamon.Web/JsLib/src/utils/index.js
+++ src/Cinnamon.Web/JsLib/src/utils/index.js
@@ -1,9 +1,7 @@
 import cookieManager from "./cookieManager.js";
 import excelWriter from "./excelWriter.js";
-import masterListToExcel from "./masterListToExcel.js"
 
 export default {
   cookieManager,
     excelWriter,
-    masterListToExcel
 };
