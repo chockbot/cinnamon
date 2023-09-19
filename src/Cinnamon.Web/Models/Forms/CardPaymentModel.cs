40c26091502d1d88a40c910301bd01ef38f026ce
Rica Namoro <ricanamoro00@gmail.com>
2023-09-19 12:46:44 +0800

    add expiry validation

--- src/Cinnamon.Web/Models/Forms/CardPaymentModel.cs
+++ src/Cinnamon.Web/Models/Forms/CardPaymentModel.cs
@@ -10,9 +10,49 @@ public class CardPaymentModel
     public string CardNumber {get; set;}
     [Required(ErrorMessage = "Required card holder name field")]
     public string CardHolder {get; set;}
-    [Required(ErrorMessage = "Required card expiration. Eg: 03/23")]
-    [CardExpiration(ErrorMessage = "Provide valid card expiration format. Eg: 03/23")]
-    public string Expiration {get; set;}
+    private string _expiration;
+
+    [Required(ErrorMessage = "Card expiration is required. Eg: 03/23")]
+    [CardExpiration(ErrorMessage = "Provide a valid card expiration format. Eg: 03/23")]
+    public string Expiration
+    {
+        get
+        {
+            // If _expiration is not empty and has at least 4 characters, format it as MM/YY
+            if (!string.IsNullOrEmpty(_expiration) && _expiration.Length >= 4)
+            {
+                return $"{_expiration.Substring(0, 2)}/{_expiration.Substring(2, 2)}";
+            }
+            else
+            {
+                // Return the cleaned input as is
+                return _expiration;
+            }
+        }
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                // Remove any non-numeric characters from the input
+                string cleanedInput = new string(value.Where(char.IsDigit).ToArray());
+
+                // Ensure the cleaned input has at least 4 characters (MMYY)
+                if (cleanedInput.Length >= 4)
+                {
+                    // Extract the first 4 characters (MMYY)
+                    _expiration = cleanedInput.Substring(0, 4);
+                }
+                else
+                {
+                    _expiration = cleanedInput; // Use the cleaned input as is
+                }
+            }
+            else
+            {
+                _expiration = value; // Handle null or empty input
+            }
+        }
+    }
     [Required(ErrorMessage = "Required card CVV field.")]
     [StringLength(4, MinimumLength = 3, ErrorMessage = "Provide valid CVV")]
     public string CVV {get; set;}
