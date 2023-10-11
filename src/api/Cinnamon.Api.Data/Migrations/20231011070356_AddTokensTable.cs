0c52f9403b70ffe8b76dda93cf6d870e7f153034
dechalico <dechalico.dev@gmail.com>
2023-10-11 15:06:08 +0800

    Add Token table

---
+++ src/api/Cinnamon.Api.Data/Migrations/20231011070356_AddTokensTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Cinnamon.Api.Data.Migrations
+{
+    public partial class AddTokensTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TokenGenerateds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TokenType = table.Column<string>(type: "text", nullable: false),
+                    Guid = table.Column<string>(type: "text", nullable: false),
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    Payload = table.Column<string>(type: "text", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<int>(type: "integer", nullable: false),
+                    ChangedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ChangedBy = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TokenGenerateds", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenGenerateds_Guid",
+                table: "TokenGenerateds",
+                column: "Guid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenGenerateds_Guid_Token",
+                table: "TokenGenerateds",
+                columns: new[] { "Guid", "Token" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TokenGenerateds");
+        }
+    }
+}
