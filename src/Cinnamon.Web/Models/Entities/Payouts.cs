e9d5c95310523f324113ba6b2e087eecf639619a
Rica Namoro <ricanamoro00@gmail.com>
2023-10-25 17:28:57 +0800

    account page ui

---
+++ src/Cinnamon.Web/Models/Entities/Payouts.cs
@@ -0,0 +1,12 @@
+﻿namespace Cinnamon.Web.Models.Entities;
+
+public class Payouts
+{
+    public int Id { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public bool Status { get; set; }
+
+    public decimal Amount { get; set; } 
+}
