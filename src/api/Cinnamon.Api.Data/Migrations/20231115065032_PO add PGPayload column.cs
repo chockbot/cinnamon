9a4ec44b17a1930cdc5b5bf5a96b95f5f630a6a4
dechalico <dechalico.dev@gmail.com>
2023-11-15 14:52:07 +0800

    api data PO add PGPayload column

---
+++ src/api/Cinnamon.Api.Data/Migrations/20231115065032_PO add PGPayload column.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinnamon.Api.Data.Migrations
+{
+    public partial class POaddPGPayloadcolumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PGPayload",
+                table: "PurchaseOrders",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PGPayload",
+                table: "PurchaseOrders");
+        }
+    }
+}
