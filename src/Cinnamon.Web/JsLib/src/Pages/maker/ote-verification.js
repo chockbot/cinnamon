721aa86b12fa35e022f97322b52589fb09d9567e
dechalico <dechalico.dev@gmail.com>
2023-11-20 16:30:01 +0800

    setup qr code scanner frontend

---
+++ src/Cinnamon.Web/JsLib/src/Pages/maker/ote-verification.js
@@ -0,0 +1,36 @@
+import { Html5Qrcode } from "html5-qrcode";
+
+class QRVerification {
+  constructor(targetId, dotnetObj) {
+    this.targetId = targetId;
+    this.dotnetObj = dotnetObj;
+    this.scanner = new Html5Qrcode(this.targetId);
+  }
+
+  startScan() {
+    const onScanSuccess = async (decodedText, decodedResult) => {
+      console.log(`Code matched = ${decodedText}`, decodedResult);
+      this.scanner.pause(true);
+      await this.dotnetObj.invokeMethodAsync("VerifyQR", decodedText);
+    };
+
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+    this.scanner.start({ facingMode: "environment" }, config, onScanSuccess);
+
+    $(`#${this.targetId}`).toggleClass("h-auto");
+  }
+
+  stopScan() {
+    this.scanner.stop();
+    $(`#${this.targetId}`).toggleClass("h-auto");
+  }
+
+  resumeScan() {
+    this.scanner.resume();
+  }
+}
+
+export default {
+  createInstance: (targetId, obj) => new QRVerification(targetId, obj),
+};
