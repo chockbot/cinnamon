e894ca889a36fc63a1c951063d4a540268ea37d2
dechalico <dechalico.dev@gmail.com>
2023-12-03 16:47:13 +0800

    api core update popular activities parameters

--- src/api/Cinnamon.Api.Core/Controllers/ActivityController.cs
+++ src/api/Cinnamon.Api.Core/Controllers/ActivityController.cs
@@ -2382,7 +2382,8 @@ public class ActivityController : ControllerBase
             var result = await popularActivitiesHandler.ExecuteAsync(new Services.ActivityService.Interactors.PopularActivitiesArgs
             {
                 Skip = args.PageIndex,
-                Take = args.CountPerPage
+                Take = args.CountPerPage,
+                CategoryId = args.CategoryId
             });
             if (!result.Succeeded || result.Result == null)
             {
--- src/api/Cinnamon.Api.Core/Services/ActivityService/Interactors/PopularActivitiesArgs.cs
+++ src/api/Cinnamon.Api.Core/Services/ActivityService/Interactors/PopularActivitiesArgs.cs
@@ -6,4 +6,5 @@ public class PopularActivitiesArgs : IInteractor
 {
     public int? Take {get; set;}
     public int? Skip {get; set;}
+    public int? CategoryId {get; set;}
 }
