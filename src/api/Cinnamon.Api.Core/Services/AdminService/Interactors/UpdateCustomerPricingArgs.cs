6e3b35522e81619bb46a305a4d35266ea0cc9a66
dechalico <dechalico.dev@gmail.com>
2023-12-10 00:39:54 +0800

    api core update customer pricing

--- src/api/Cinnamon.Api.Core/Services/AdminService/Interactors/UpdateCustomerPricingArgs.cs
+++ src/api/Cinnamon.Api.Core/Services/AdminService/Interactors/UpdateCustomerPricingArgs.cs
@@ -7,4 +7,5 @@ public class UpdateCustomerPricingArgs : IInteractor
     public int CustomerId {get; set;}
     public decimal Rate {get; set;}
     public bool IsManualPayment {get; set;}
+    public bool InclusivePricing {get; set;}
 }
