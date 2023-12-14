ee50bb030f8a9b3bf91731ade677146a6964f0f8
dechalico <dechalico.dev@gmail.com>
2023-12-14 15:54:50 +0800

    api core add oteperday service

--- src/api/Cinnamon.Api.Core/Modules/DataAccess/Handlers/IActivityData.cs
+++ src/api/Cinnamon.Api.Core/Modules/DataAccess/Handlers/IActivityData.cs
@@ -25,4 +25,5 @@ public interface IActivityData
     Task<AppResult<AddTicketSoldResult>> AddTicketSolds(AddTicketSoldArgs args);
     Task<AppResult<GetOTEByProvideResult>> GetOTEByProvider(GetOTEByProvideArgs args);
     Task<AppResult<CustomerOteResult>> CustomerOte(int customerId);
+    Task<AppResult<OtePerDateResult>> OtePerDate(OtePerDateArgs args);
 }
