303931616c08493dd5a277f4db543611f0c7ba24
Rica Namoro <rica.namoro@cinnamon.ph>
2024-01-15 17:01:01 +0800

    Gen-XP -Contact Me

--- src/Cinnamon.Web/JsLib/src/Pages/activity/index.js
+++ src/Cinnamon.Web/JsLib/src/Pages/activity/index.js
@@ -9,6 +9,7 @@ import moveImage from "./moveImage";
 import carouselSwipe from "./carouselSwipe";
 import explore from "./explore";
 import activityDetails from "./activityDetails";
+import { showTypeForm } from "./showTypeForm";
 
 export default {
   creation,
@@ -22,4 +23,5 @@ export default {
   carouselSwipe,
   explore,
   activityDetails,
+  showTypeForm
 };
---
+++ src/Cinnamon.Web/JsLib/src/Pages/activity/showTypeForm.js
@@ -0,0 +1,18 @@
+﻿import { createWidget } from '@typeform/embed'
+
+export function showTypeForm() {
+    // Replace 'cgzRQKWI' with your actual Typeform ID
+    var typeformId = 'cgzRQKWI';
+    
+    // Open Typeform popup
+    window.typeformEmbed.makePopup(typeformId, {
+        mode: 'popup',
+        autoOpen: true,
+        autoClose: 0,
+        refresh: true,
+    });
+}
+
+export default {
+    showTypeForm
+};
