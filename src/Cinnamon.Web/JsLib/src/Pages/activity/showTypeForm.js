452bfe52172b817486ac082d3c13db71c9eedbb8
Rica Namoro <rica.namoro@cinnamon.ph>
2024-01-16 14:04:55 +0800

    remove showTypeForm.js

--- src/Cinnamon.Web/JsLib/src/Pages/activity/showTypeForm.js
+++
@@ -1,5 +0,0 @@
-﻿export function integrateTypeform() {
-    const { open, close, toggle, refresh } = window.tf.createPopup("cgzRQKWI");
-    document.querySelector("#typeFormBtn").onclick = toggle;
-}
-
