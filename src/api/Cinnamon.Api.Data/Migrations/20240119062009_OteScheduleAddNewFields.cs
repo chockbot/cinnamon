74d1a0a97313e0629907d596f8ae70d655f010f5
dechalico <dechalico.dev@gmail.com>
2024-01-19 14:21:06 +0800

    add migration

---
+++ src/api/Cinnamon.Api.Data/Migrations/20240119062009_OteScheduleAddNewFields.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinnamon.Api.Data.Migrations
+{
+    public partial class OteScheduleAddNewFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EventDurationCount",
+                table: "OteSchedules",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EventDurationTimeUnit",
+                table: "OteSchedules",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventDurationCount",
+                table: "OteSchedules");
+
+            migrationBuilder.DropColumn(
+                name: "EventDurationTimeUnit",
+                table: "OteSchedules");
+        }
+    }
+}
