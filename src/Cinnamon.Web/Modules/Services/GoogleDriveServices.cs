4807c62a7f30e6fc7b5d15d756bd6f911fdd75d1
Rica Namoro <rica.namoro@cinnamon.ph>
2024-02-15 09:07:44 +0800

    Materials Page

--- src/Cinnamon.Web/Modules/Services/GoogleDriveServices.cs
+++ src/Cinnamon.Web/Modules/Services/GoogleDriveServices.cs
@@ -2,6 +2,9 @@
 using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using Google.Apis.Drive.v3.Data;
+using Cinnamon.Web.Models.Entities;
+
 
 namespace Cinnamon.Web.Modules.Services;
 
@@ -14,60 +17,92 @@ public class GoogleDriveService
     {
         // Read Google Drive credentials from appsettings.json
         _configuration = configuration;
-
-        var clientId = _configuration["AppConfig:Authentication:Google:ClientId"];
-        var clientSecret = _configuration["AppConfig:Authentication:Google:ClientSecret"];
+        var credentialFilePath = _configuration["AppConfig:Authentication:Google:ServicePath"];
 
         // Specify the scopes required for accessing Google Drive
         string[] scopes = { DriveService.Scope.Drive };
 
-        // Initialize Google credentials using OAuth 2.0
-        var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-            new ClientSecrets
-            {
-                ClientId = clientId,
-                ClientSecret = clientSecret
-            },
-            scopes, 
-            "user",
-            CancellationToken.None,
-            new FileDataStore("Cinnamon.GoogleDrive.Auth.Store")).Result;
-
-        // Once authorized, retrieve access token and refresh token
-        var accessToken = credential.Token.AccessToken;
-        var refreshToken = credential.Token.RefreshToken;
-
-        // Initialize the DriveService using the credentials
-        var service = new DriveService(new BaseClientService.Initializer
+        // Initialize Google credentials using service account
+        var credential = GoogleCredential.FromFile(credentialFilePath)
+                                          .CreateScoped(scopes);
+        // Initialize the DriveService using the service account credentials
+        _driveService = new DriveService(new BaseClientService.Initializer
         {
             HttpClientInitializer = credential,
             ApplicationName = "Cinnamon"
         });
     }
 
-    public async Task<List<object>> ListFiles()
+    public async Task<List<Models.Entities.File>> ListFilesInFolder()
     {
-        var files = new List<object>();
-        // Define parameters for the file list request
+        var files = new List<Models.Entities.File>();
         var listRequest = _driveService.Files.List();
-        listRequest.Q = "parent in '12XSwbW7YvRNLz39U63L9UZDgiHEpTYO_'";
+        listRequest.Q = $"'1A5iZBZWoF9g3m3F0p8LnmnQcxdANmEWf' in parents"; // Search for files in the given folder
+        listRequest.Fields = "files(id, name, mimeType, thumbnailLink)";
 
-        listRequest.Fields = "files(id, name, type)";
-
-        // Execute the request asynchronously
         var result = await listRequest.ExecuteAsync();
 
-        // Extract file information from the result
         foreach (var file in result.Files)
         {
-            files.Add(new
+            files.Add(new Models.Entities.File
             {
                 Id = file.Id,
                 Name = file.Name,
-                Type = file.MimeType
-
+                Type = file.MimeType,
+                ThumbnailLink = file.ThumbnailLink
             });
         }
+
+        return files;
+    }
+    public async Task<byte[]> GetFileContent(string fileId)
+    {
+        var getRequest = _driveService.Files.Get(fileId);
+        var stream = new MemoryStream();
+        await getRequest.DownloadAsync(stream);
+        return stream.ToArray();
+    }
+    public async Task<Models.Entities.File> GetFileInfo(string fileId)
+    {
+        var getRequest = _driveService.Files.Get(fileId);
+        getRequest.Fields = "id, name, mimeType, thumbnailLink"; // Specify the fields to retrieve
+
+        var file = await getRequest.ExecuteAsync();
+        return new Models.Entities.File
+        {
+            Id = file.Id,
+            Name = file.Name,
+            Type = file.MimeType,
+            ThumbnailLink = file.ThumbnailLink
+        };
+    }
+    public async Task<List<Models.Entities.File>> SearchFile(string searchQuery)
+    {
+        var files = new List<Models.Entities.File>();
+        var listRequest = _driveService.Files.List();
+        listRequest.Q = $"'1A5iZBZWoF9g3m3F0p8LnmnQcxdANmEWf' in parents"; // Search for files in the given folder
+
+        if (!string.IsNullOrEmpty(searchQuery))
+        {
+            // Add search query to filter by file name
+            listRequest.Q += $" and name contains '{searchQuery}'";
+        }
+
+        listRequest.Fields = "files(id, name, mimeType, thumbnailLink)";
+
+        var result = await listRequest.ExecuteAsync();
+
+        foreach (var file in result.Files)
+        {
+            files.Add(new Models.Entities.File
+            {
+                Id = file.Id,
+                Name = file.Name,
+                Type = file.MimeType,
+                ThumbnailLink = file.ThumbnailLink
+            });
+        }
+
         return files;
     }
 }
