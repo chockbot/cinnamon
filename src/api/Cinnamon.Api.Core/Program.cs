1713b5eb3e35b9c32759b34e525b70de302a21c9
dechalico <dechalico.dev@gmail.com>
2024-02-23 11:11:50 +0800

    temporary disable unread message notification

--- src/api/Cinnamon.Api.Core/Program.cs
+++ src/api/Cinnamon.Api.Core/Program.cs
@@ -151,13 +151,13 @@ builder.Services.AddQuartz(q => {
         );
     }
 
-    var unreadMessagesNotificationKey = new JobKey("GenerateUnreadChatsNotificationJob");
-    q.AddJob<GenerateUnreadChatsNotificationJob>(opts => opts.WithIdentity(unreadMessagesNotificationKey));
-    q.AddTrigger(opts => opts
-        .ForJob(unreadMessagesNotificationKey)
-        .WithIdentity("GenerateUnreadChatsNotificationJob-trigger")
-        .WithSimpleSchedule(x => x.WithIntervalInHours(1).RepeatForever())
-    );
+    // var unreadMessagesNotificationKey = new JobKey("GenerateUnreadChatsNotificationJob");
+    // q.AddJob<GenerateUnreadChatsNotificationJob>(opts => opts.WithIdentity(unreadMessagesNotificationKey));
+    // q.AddTrigger(opts => opts
+    //     .ForJob(unreadMessagesNotificationKey)
+    //     .WithIdentity("GenerateUnreadChatsNotificationJob-trigger")
+    //     .WithSimpleSchedule(x => x.WithIntervalInHours(1).RepeatForever())
+    // );
 });
 builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
 
