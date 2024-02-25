735b7df81fbcd13a153a6873de5cd4bcd4bd48f2
Rica Namoro <rica.namoro@cinnamon.ph>
2024-02-26 07:55:03 +0800

    material page adjustments

--- src/Cinnamon.Web/wwwroot/js/PrintFile.js
+++ src/Cinnamon.Web/wwwroot/js/PrintFile.js
@@ -3,30 +3,41 @@
         var img = new Image();
         img.src = 'data:' + fileType + ';base64,' + base64Content;
         img.onload = function () {
-            var printWindow = window;
+            var printWindow = window.open('', '_blank'); // Open in a new tab
             printWindow.document.open();
             printWindow.document.write('<html><head><style>@page { size: A4; margin: 0; }</style><title>Print Image</title></head><body>');
             printWindow.document.write('<img src="' + img.src + '" style="max-width: 100%; height: auto;" />');
             printWindow.document.write('</body></html>');
             printWindow.document.close();
-            printWindow.print();
+
+            // Add event listener to close print window if print dialog is canceled
+            window.addEventListener('beforeprint', function () {
+                printWindow.close();
+            });
             // Reload the page after print dialog is closed or canceled
             window.addEventListener('afterprint', function () {
                 location.reload(true);
             });
+
+            printWindow.print();
         };
     } else {
         var binaryContent = atob(base64Content);
         var blob = new Blob([new Uint8Array(binaryContent.length).map((_, i) => binaryContent.charCodeAt(i))], { type: fileType });
         var url = URL.createObjectURL(blob);
-        var printWindow = window.open(url, '_self');
+        var printWindow = window.open(url, '_blank'); // Open in a new tab
         printWindow.onload = function () {
-            printWindow.print();
-            URL.revokeObjectURL(url);
+            // Add event listener to close print window if print dialog is canceled
+            window.addEventListener('beforeprint', function () {
+                printWindow.close();
+            });
             // Reload the page after print dialog is closed or canceled
-            printWindow.addEventListener('afterprint', function () {
+            window.addEventListener('afterprint', function () {
                 location.reload(true);
             });
+
+            printWindow.print();
+            URL.revokeObjectURL(url);
         };
     }
 }
