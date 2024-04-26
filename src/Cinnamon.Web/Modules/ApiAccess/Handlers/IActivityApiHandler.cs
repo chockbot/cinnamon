1e53ae824285dcc2fe1abd46ad481b6154538e16
dechalico <dechalico.dev@gmail.com>
2024-04-26 15:23:26 +0800

    web app mapped api core endpoing

--- src/Cinnamon.Web/Modules/ApiAccess/Handlers/IActivityApiHandler.cs
+++ src/Cinnamon.Web/Modules/ApiAccess/Handlers/IActivityApiHandler.cs
@@ -64,4 +64,5 @@ public interface IActivityApiHandler
     Task<AppResult<DeleteOnlineEventResult>> DeleteOnlineEvent(DeleteOnlineEventArgs args, string token);
     Task<AppResult<UpdateSharedLinkStatusResult>> UpdateSharedLinkStatus(UpdateSharedLinkStatusArgs args, string token);
     Task<AppResult<ActivityFeedResult>> ActivityFeed(ActivityFeedArgs args);
+    Task<AppResult<OteAlreadyBookedDatesResult>> OteAlreadyBookedDates(int activityId, string token);
 }
