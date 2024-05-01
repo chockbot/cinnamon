b0808ae5ff165481e61938ef2fc2e7fa333658b1
Rica Namoro <rica.namoro@cinnamon.ph>
2024-05-02 07:59:36 +0800

    fix

--- src/Cinnamon.Web/Modules/ApiAccess/Activity/ActivityApiHandler.cs
+++ src/Cinnamon.Web/Modules/ApiAccess/Activity/ActivityApiHandler.cs
@@ -1200,28 +1200,6 @@ public class ActivityApiHandler : IActivityApiHandler
             return AppResult<UpdateSharedLinkStatusResult>.CreateFailed(ex, "An error occurred when calling generate event shared link api");
         }
     }
-
-    public async Task<AppResult<ActivityFeedResult>> ActivityFeed(ActivityFeedArgs args)
-    {
-        try
-        {
-            var result = await flurlClient
-                .Request($"Activity/ActivityFeed")
-                .SetQueryParams(args)
-                .GetJsonAsync<ActivityFeedResult>();
-
-            return AppResult<ActivityFeedResult>.CreateSucceeded(result, "Successfully get activity feed.");
-        }
-        catch (FlurlHttpException ex)
-        {
-            return AppResult<ActivityFeedResult>.CreateFailed(ex, ex.Message);
-        }
-        catch (Exception ex)
-        {
-            return AppResult<ActivityFeedResult>.CreateFailed(ex, "An error occured when get activity feed.");
-        }
-    }
-
     public async Task<AppResult<DeleteTicketResult>> DeleteTicket(DeleteTicketArgs args, string token)
     {
         try
