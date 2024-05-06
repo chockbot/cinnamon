44fc8171d3c9fbd960504fbb02ceb2388bb21ef4
dechalico <dechalico.dev@gmail.com>
2024-05-06 18:04:40 +0800

    category filter ui enhancement

--- src/Cinnamon.Web/JsLib/src/Pages/maker/attendance.js
+++ src/Cinnamon.Web/JsLib/src/Pages/maker/attendance.js
@@ -1,73 +1,67 @@
 export function initSwiper(selector, scrollbarSelector) {
-    const swiper = new Swiper(selector, {
-        slidesPerView: "auto",
-        spaceBetween: 30,
-        grabCursor: true,
-        keyboard: {
-            enabled: true,
-        },
-        scrollbar: {
-            el: ".swiper-scrollbar",
-        },
-        observer: true,
-        observeParents: true,
-    });
+  const swiper = new Swiper(selector, {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
 }
 
 export function initSwiperHelpPage(selector) {
-    const swiper = new Swiper(selector, {
-        slidesPerView: "auto",
-        spaceBetween: 30,
-        grabCursor: true,
-        keyboard: {
-            enabled: true,
-        },
-        scrollbar: false,
-        observer: true,
-        observeParents: true,
-    });
+  const swiper = new Swiper(selector, {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    scrollbar: false,
+    observer: true,
+    observeParents: true,
+  });
 }
 
 export function initCircularProgress(selector, percentage) {
-    $(selector).each(function () {
-        const left = $(this).find(".progress-left .progress-bar");
-        const right = $(this).find(".progress-right .progress-bar");
+  $(selector).each(function () {
+    const left = $(this).find(".progress-left .progress-bar");
+    const right = $(this).find(".progress-right .progress-bar");
 
-        if (percentage <= 50) {
-            right.css(
-                "transform",
-                "rotate(" + percentageToDegrees(percentage) + "deg)"
-            );
-            if (percentage === 0) {
-                left.css(
-                    "transform",
-                    "rotate(" + percentageToDegrees(percentage) + "deg)"
-                );
-            }
-        }
-        else {
-            right.css("transform", "rotate(180deg)");
-            left.css(
-                "transform",
-                "rotate(" + percentageToDegrees(percentage - 50) + "deg)"
-            );
-            if (percentage > 100) {
-                right.css("transform", "rotate(180deg)");
-                left.css(
-                    "transform",
-                    "rotate(" + percentageToDegrees(50) + "deg)"
-                );
-            }
-        }
-    });
-
-    function percentageToDegrees(percentage) {
-        return (percentage / 100) * 360;
+    if (percentage <= 50) {
+      right.css(
+        "transform",
+        "rotate(" + percentageToDegrees(percentage) + "deg)"
+      );
+      if (percentage === 0) {
+        left.css(
+          "transform",
+          "rotate(" + percentageToDegrees(percentage) + "deg)"
+        );
+      }
+    } else {
+      right.css("transform", "rotate(180deg)");
+      left.css(
+        "transform",
+        "rotate(" + percentageToDegrees(percentage - 50) + "deg)"
+      );
+      if (percentage > 100) {
+        right.css("transform", "rotate(180deg)");
+        left.css("transform", "rotate(" + percentageToDegrees(50) + "deg)");
+      }
     }
+  });
+
+  function percentageToDegrees(percentage) {
+    return (percentage / 100) * 360;
+  }
 }
 
 export default {
-    initSwiper,
-    initSwiperHelpPage,
-    initCircularProgress,
+  initSwiper,
+  initSwiperHelpPage,
+  initCircularProgress,
 };
