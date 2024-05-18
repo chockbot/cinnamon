fcb0684dd34f9f61e7f6e7505ff02fa62f731922
dechalico <dechalico.dev@gmail.com>
2024-05-19 05:56:43 +0800

    add order by in generic entity

--- src/api/Cinnamon.Api.Data/Repository/DbSets/GenericEntity.cs
+++ src/api/Cinnamon.Api.Data/Repository/DbSets/GenericEntity.cs
@@ -55,7 +55,11 @@ public class GenericEntity<TTarget> : IGenericEntity<TTarget> where TTarget : Ba
             int limitCount = take.HasValue ? take.Value : int.MaxValue;
             int skipCount = skip.HasValue ? skip.Value : 0;
 
-            var query = applicationContext.Set<TTarget>().Where(expression).Skip(skipCount).Take(limitCount);
+            var query = applicationContext.Set<TTarget>()
+                            .Where(expression)
+                            .OrderBy(o => o.Id)
+                            .Skip(skipCount)
+                            .Take(limitCount);
 
             if(includes != null)
             {
