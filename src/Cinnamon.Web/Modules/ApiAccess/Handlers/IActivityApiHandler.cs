2642c3a24f2475e914343f7d3be26c2a1fe14062
dechalico <dechalico.dev@gmail.com>
2024-05-19 05:23:31 +0800

    map to iterface

--- src/Cinnamon.Web/Modules/ApiAccess/Handlers/IActivityApiHandler.cs
+++ src/Cinnamon.Web/Modules/ApiAccess/Handlers/IActivityApiHandler.cs
@@ -66,4 +66,5 @@ public interface IActivityApiHandler
     Task<AppResult<DeleteTicketResult>> DeleteTicket(DeleteTicketArgs args, string token);
     Task<AppResult<ActivityFeedResult>> ActivityFeed(ActivityFeedArgs args);
     Task<AppResult<OteAlreadyBookedDatesResult>> OteAlreadyBookedDates(int activityId, string token);
+    Task<AppResult<OteScheduleDatesResult>> OteScheduleDates(OteScheduleDatesArgs args, string token);
 }
