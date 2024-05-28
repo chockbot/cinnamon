37d43d88dcc277cb390c607f399cef4508c3d8f0
dechalico <dechalico.dev@gmail.com>
2024-05-29 00:11:24 +0800

    api core direct student info

--- src/api/Cinnamon.Api.Core/Extensions/ServiceExtension.cs
+++ src/api/Cinnamon.Api.Core/Extensions/ServiceExtension.cs
@@ -282,6 +282,7 @@ public static class ServiceExtenstion
         // direct students services
         services.AddTransient<Services.DirectStudentService.Handlers.IDirectStudentsInfoHandler, Services.DirectStudentService.DirectStudentsInfoHandler>();
         services.AddTransient<Services.DirectStudentService.Handlers.IUpdateDirectStudentHandler, Services.DirectStudentService.UpdateDirectStudentHandler>();
+        services.AddTransient<Services.DirectStudentService.Handlers.IDirectStudentHandler, Services.DirectStudentService.DirectStudentHandler>();
 
         return services;
     }
---
+++ src/api/Cinnamon.Api.Core/Services/DirectStudentService/Interactors/DirectStudentArgs.cs
@@ -0,0 +1,8 @@
+using Cinnamon.Framework.Interactor;
+
+namespace Cinnamon.Api.Core.Services.DirectStudentService.Interactors;
+
+public class DirectStudentArgs : IInteractor 
+{
+    public int StudentId {get; set;}
+}
