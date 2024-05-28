7f90a1d3e6020ccbf8b6c46f1cdaa0bd219788e4
dechalico <dechalico.dev@gmail.com>
2024-05-29 01:05:27 +0800

    update direct student enhancement

--- src/api/Cinnamon.Api.Core/Services/DirectStudentService/UpdateDirectStudentHandler.cs
+++ src/api/Cinnamon.Api.Core/Services/DirectStudentService/UpdateDirectStudentHandler.cs
@@ -1,5 +1,6 @@
 using Cinnamon.Api.Core.Modules.DataAccess.Handlers;
 using Cinnamon.Api.Core.Services.AccountService.Handlers;
+using Cinnamon.Api.Core.Services.ActivityService.Handlers;
 using Cinnamon.Api.Core.Services.DirectStudentService.Handlers;
 using Cinnamon.Api.Core.Services.DirectStudentService.Interactors;
 using Cinnamon.Api.Core.Services.DirectStudentService.Interactors.Results;
@@ -11,11 +12,16 @@ public class UpdateDirectStudentHandler : IUpdateDirectStudentHandler
 {
     private readonly IDirectStudentData directStudentData;
     private readonly IGetProfileHandler getProfileHandler;
+    private readonly IDirectStudentHandler directStudentHandler;
+    private readonly IGetActivityHandler getActivityHandler;
 
-    public UpdateDirectStudentHandler(IGetProfileHandler getProfileHandler, IDirectStudentData directStudentData)
+    public UpdateDirectStudentHandler(IGetProfileHandler getProfileHandler, IDirectStudentData directStudentData,
+        IDirectStudentHandler directStudentHandler, IGetActivityHandler getActivityHandler)
     {
         this.directStudentData = directStudentData;
         this.getProfileHandler = getProfileHandler;
+        this.directStudentHandler = directStudentHandler;
+        this.getActivityHandler = getActivityHandler;
     }
     
     public AppResult<UpdateDirectStudentResult> Execute(UpdateDirectStudentArgs args)
@@ -35,6 +41,49 @@ public class UpdateDirectStudentHandler : IUpdateDirectStudentHandler
                     new ApplicationException("Students must be at least 4 years old."), "Students must be at least 4 years old.");
             }
 
+            var directStudentRes = await directStudentHandler.ExecuteAsync(new DirectStudentArgs {
+                StudentId = args.StudentId
+            });
+            if(!directStudentRes.Succeeded || directStudentRes.Result is null)
+            {
+                return AppResult<UpdateDirectStudentResult>.CreateFailed(new ApplicationException(directStudentRes.Message), directStudentRes.Message);
+            }
+
+            int? numberOfSessions = null;
+            if(args.ActivityId.HasValue || args.ScheduleId.HasValue)
+            {
+                var activityRes = await getActivityHandler.ExecuteAsync(new ActivityService.Interactors.GetActivityArgs {
+                    ActivityId = args.ActivityId ?? 0,
+                    IncludeAtivitySchedules = true,
+                    IncludeCustomer = true
+                });
+                if(!activityRes.Succeeded || activityRes.Result is null)
+                {
+                    return AppResult<UpdateDirectStudentResult>.CreateFailed( new ApplicationException(activityRes.Message), activityRes.Message);
+                }
+
+                var profleRes = await getProfileHandler.ExecuteAsync(new AccountService.Interactors.GetProfileArgs {});
+                if(!profleRes.Succeeded || profleRes.Result is null)
+                {
+                    return AppResult<UpdateDirectStudentResult>.CreateFailed( new ApplicationException(profleRes.Message), profleRes.Message);
+                }
+
+                if(profleRes.Result.Id != activityRes.Result.Owner?.Id)
+                {
+                    return AppResult<UpdateDirectStudentResult>.CreateFailed( 
+                        new ApplicationException("Action not allowed. Invalid request."), "Action not allowed. Invalid request.");
+                }
+
+                var schedule = activityRes.Result.ActivitySchedules.FirstOrDefault(s => s.Id == args.ScheduleId);
+                if(schedule is null)
+                {
+                    return AppResult<UpdateDirectStudentResult>.CreateFailed( 
+                        new ApplicationException("Action not allowed. Invalid request."), "Action not allowed. Invalid request.");
+                }
+
+                numberOfSessions = schedule.PerUnit2;
+            }
+
             var result = await directStudentData.UpdateDirectStudent(new Framework.ApiCommand.ApiData.DirectStudent.Request.UpdateDirectStudentArgs {
                 UpdateDirectStudentData = new Framework.ApiCommand.ApiData.DirectStudent.Request.UpdateDirectStudentArgs.UpdateDirectStudent {
                     UpdateDirectStudentInfo = new Framework.ApiCommand.ApiData.DirectStudent.Request.UpdateDirectStudentArgs.UpdateDirectStudentInfo {
@@ -51,9 +100,9 @@ public class UpdateDirectStudentHandler : IUpdateDirectStudentHandler
                         ActivityId = args.ActivityId,
                         ScheduleId = args.ScheduleId,
                         Name = args.Name,
-                        NumberOfSessions = args.NumberOfSessions,
+                        NumberOfSessions = numberOfSessions,
                         Remarks = args.Remarks,
-                        StudentNo = args.StudentNo
+                        StudentNo = args.StudentNo,
                     }
                 }
             });
