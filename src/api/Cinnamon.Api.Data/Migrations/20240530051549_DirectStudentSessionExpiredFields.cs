57da6e42c287aa00168912acdc0de95b5f552dfe
dechalico <dechalico.dev@gmail.com>
2024-05-30 13:17:40 +0800

    add direct student expired field migration

---
+++ src/api/Cinnamon.Api.Data/Migrations/20240530051549_DirectStudentSessionExpiredFields.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinnamon.Api.Data.Migrations
+{
+    public partial class DirectStudentSessionExpiredFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpirationDateEnd",
+                table: "DirectStudentSessions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpirationDateStart",
+                table: "DirectStudentSessions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpirationDateEnd",
+                table: "DirectStudentSessions");
+
+            migrationBuilder.DropColumn(
+                name: "ExpirationDateStart",
+                table: "DirectStudentSessions");
+        }
+    }
+}
