c746a3b17994fadc53193a2141af2ebdfa2d430e
dechalico <dechalico.dev@gmail.com>
2024-06-13 22:24:25 +0800

    add migrations

---
+++ src/api/Cinnamon.Api.Data/Migrations/20240613141225_DirectStudentAddEmailPaymentDate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinnamon.Api.Data.Migrations
+{
+    public partial class DirectStudentAddEmailPaymentDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaymentDate",
+                table: "DirectStudentPayments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "DirectStudentInfos",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentDate",
+                table: "DirectStudentPayments");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "DirectStudentInfos");
+        }
+    }
+}
