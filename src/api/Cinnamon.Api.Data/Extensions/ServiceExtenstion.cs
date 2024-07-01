9091de09b6389a91d5c9bdc2511db1f1e3511086
dechalico <dechalico.dev@gmail.com>
2024-07-01 18:22:54 +0800

    provider custom repository enhancement

--- src/api/Cinnamon.Api.Data/Extensions/ServiceExtenstion.cs
+++ src/api/Cinnamon.Api.Data/Extensions/ServiceExtenstion.cs
@@ -86,6 +86,7 @@ public static class ServiceExtenstion
         services.AddTransient<Services.Repository.Interfaces.IDynamicEmailTemplateRepository, Services.Repository.DynamicEmailTemplate.DynamicEmailTemplateRepository>();
         services.AddTransient<Services.Repository.Interfaces.IOteWaitlistRepository, Services.Repository.OteWaitlist.OteWaitlistRepository>();
         services.AddTransient<Services.Repository.Interfaces.IOteReminderRepository, Services.Repository.OteReminderFlag.OteReminderFlagRepository>();
+        services.AddTransient<Services.Repository.Interfaces.IProviderCustomQuestionRepository, Services.Repository.ProviderCustomQuestion.ProviderCustomQuestionRepository>();
 
         return services;
     }
