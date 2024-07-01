5e42487a956f57efbe594e9109d79fef8a5355a5
dechalico <dechalico.dev@gmail.com>
2024-07-01 19:09:40 +0800

    api data provider custom questions

---
+++ src/Cinnamon.Framework/ApiCommand/ApiData/ProviderCustomQuestion/Response/CreateCustomQuestionResult.cs
@@ -0,0 +1,6 @@
+using Cinnamon.Framework.ApiCommand.ApiData.DTO.ProviderCustomQuestion;
+
+namespace Cinnamon.Framework.ApiCommand.ApiData.ProviderCustomQuestion.Response;
+
+public class CreateCustomQuestionResult : AbstractModel<ProviderCustomQuestionDTO>
+{}
