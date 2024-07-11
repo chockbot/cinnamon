a395d1026d26153532b7df64eb97326abc3fa769
dechalico <dechalico.dev@gmail.com>
2024-07-11 17:10:26 +0800

    api core endpoint for top booked customers

--- src/api/Cinnamon.Api.Core/Controllers/ActivityController.cs
+++ src/api/Cinnamon.Api.Core/Controllers/ActivityController.cs
@@ -91,6 +91,7 @@ public class ActivityController : ControllerBase
     private readonly IDeleteOteWaitlistHandler deleteOteWaitlistHandler;
     private readonly IEmailTemplateHandler emailTemplateHandler;
     private readonly IProviderQuestionsHandler providerQuestionsHandler;
+    private readonly ITopBookedCustomersHandler topBookedCustomersHandler;
 
     public ActivityController(ICreateActivityHandler createActivityHandler, IGetExperienceTypesHandler getExperienceTypesHandler,
         IGetExperienceCategoriesHandler getExperienceCategoriesHandler, IGetSubCategoriesHandler getSubCategoriesHandler,
@@ -121,7 +122,8 @@ public class ActivityController : ControllerBase
         IOteTicketBookedCountHandler oteTicketBookedCountHandler, IOteScheduleDatesHandler oteScheduleDatesHandler,
         ICreateOteWaitlistHandler createOteWaitlistHandler, IGetOteWaitlistByProviderHandler getOteWaitlistByProviderHandler, 
         IDeleteOteWaitlistHandler deleteOteWaitlistHandler,IEmailTemplateHandler emailTemplateHandler,
-        IProviderQuestionsHandler providerQuestionsHandler, IUpdateOteWaitlistHandler updateOteWaitlistHandler)
+        IProviderQuestionsHandler providerQuestionsHandler, IUpdateOteWaitlistHandler updateOteWaitlistHandler,
+        ITopBookedCustomersHandler topBookedCustomersHandler)
     {
         _logger = logger;
 
@@ -191,6 +193,7 @@ public class ActivityController : ControllerBase
         this.deleteOteWaitlistHandler = deleteOteWaitlistHandler;
         this.providerQuestionsHandler = providerQuestionsHandler;
         this.updateOteWaitlistHandler = updateOteWaitlistHandler;
+        this.topBookedCustomersHandler = topBookedCustomersHandler;
     }
 
     [Route("CreateActivity")]
@@ -3577,6 +3580,46 @@ public class ActivityController : ControllerBase
         catch (Exception ex)
         {
             return new JsonResult(new ProviderQuestionsResult { ErrorInfo = new ErrorInfo { Message = ex.Message } });
+        }
+    }
+
+    [AllowAnonymous]
+    [Route("TopBookedCustomers")]
+    [HttpGet]
+    [ProducesResponseType(typeof(TopBookedCustomersResult), StatusCodes.Status200OK)]
+    public async Task<IActionResult> TopBookedCustomers([FromQuery] TopBookedCustomersArgs args)
+    {
+        try
+        {
+            DateTime selectedDate = DateTime.ParseExact(args.BookedDate, "yyyyMMddHHmmss", CultureInfo.InvariantCulture);
+
+            var result = await topBookedCustomersHandler.ExecuteAsync(new Services.ActivityService.Interactors.TopBookedCustomersArgs {
+                ActivityId = args.ActivityId,
+                BookedDate = selectedDate
+            });
+
+            if (!result.Succeeded || result.Result is null)
+            {
+                return new JsonResult(new TopBookedCustomersResult { ErrorInfo = new ErrorInfo { Message = result.Message } });
+            }
+
+            return new JsonResult(new TopBookedCustomersResult
+            {
+                IsSuccess = true,
+                Result = new TopBookedCustomersDTO {
+                    TopBooked = result.Result.TopBooked.Select(b => new CoreDto.Customer.BasicProfileDTO {
+                        Email = b.Email,
+                        FirstName = b.FirstName,
+                        LastName = b.LastName,
+                        ProfileImage = b.ProfileImage
+                    }),
+                    TotalBooked = result.Result.TotalBooked
+                },
+            });
+        }
+        catch (Exception ex)
+        {
+            return new JsonResult(new TopBookedCustomersResult { ErrorInfo = new ErrorInfo { Message = ex.Message } });
         }
     }
 }
