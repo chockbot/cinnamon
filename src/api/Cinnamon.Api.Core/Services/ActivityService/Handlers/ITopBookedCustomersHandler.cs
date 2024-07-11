f21491fb96358704ac98a858b272e5cf230850a5
dechalico <dechalico.dev@gmail.com>
2024-07-11 16:35:46 +0800

    create handler for top booked customers

---
+++ src/api/Cinnamon.Api.Core/Services/ActivityService/Handlers/ITopBookedCustomersHandler.cs
@@ -0,0 +1,11 @@
+using Cinnamon.Api.Core.Services.ActivityService.Interactors;
+using Cinnamon.Api.Core.Services.ActivityService.Interactors.Results;
+using Cinnamon.Framework.Common;
+using Cinnamon.Framework.Interactor;
+
+namespace Cinnamon.Api.Core.Services.ActivityService.Handlers;
+
+public interface ITopBookedCustomersHandler : IInteractorHandler<TopBookedCustomersArgs,AppResult<TopBookedCustomersResult>> 
+{
+
+}
