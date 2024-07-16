407fc27d3d3c48fefe36dd7e8fe877284053789d
dechalico <dechalico.dev@gmail.com>
2024-07-17 01:19:35 +0800

    api core approve waitlist handler

--- src/api/Cinnamon.Api.Core/Extensions/ServiceExtension.cs
+++ src/api/Cinnamon.Api.Core/Extensions/ServiceExtension.cs
@@ -218,6 +218,7 @@ public static class ServiceExtenstion
         services.AddTransient<Services.TransactionService.Handlers.IGetDirectStudentSalesHandler, Services.TransactionService.GetDirectStudentSalesHandler>();
         services.AddTransient<Services.TransactionService.Handlers.IApprovedFreeWaitListHandler, Services.TransactionService.ApprovedFreeWaitListHandler>();
         services.AddTransient<Services.TransactionService.Handlers.IApprovedPaidWaitListHandler, Services.TransactionService.ApprovedPaidWaitListHandler>();
+        services.AddTransient<Services.TransactionService.Handlers.IApprovedWaitListHandler, Services.TransactionService.ApprovedWaitListHandler>();
 
         // dashboard services
         services.AddTransient<Services.DashboardService.Handlers.IGetActivitySchedulesHandler, Services.DashboardService.GetActivityScheduleHandler>();
