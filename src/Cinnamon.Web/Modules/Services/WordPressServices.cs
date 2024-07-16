79721f6eddbe6052a9de49aa1688aafdf1845e1d
TartPops6 <pikuraonmyoji@gmail.com>
2024-07-16 14:21:06 +0800

    updated some of the footer

--- src/Cinnamon.Web/Modules/Services/WordPressServices.cs
+++ src/Cinnamon.Web/Modules/Services/WordPressServices.cs
@@ -1,27 +1,58 @@
 using Flurl.Http;
+using Flurl.Http.Configuration;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Cinnamon.Web.Models.Entities;
+using Cinnamon.Framework.Common;
+using Cinnamon.Web.Models.WordPress;
+using Cinnamon.Web.Modules.Services.Handlers;
 
-namespace Cinnamon.Web.Modules.Services
-{
-    public class WordPressServices
+namespace Cinnamon.Web.Modules.Services;
+
+    public class WordPressServices : IWordPressServicesHandler
     {
-        private const string WordPressAPI = "https://cinnamon.ph/blog/wp-json/wp/v2";
+        private readonly IFlurlClient flurlClient;
+        private readonly ILogger _logger;
 
-        public async Task<List<WordPressPost>> GetPostsAsync()
+        public WordPressServices(IFlurlClientFactory flurlFac, Config.Config config, ILogger<WordPressServices> logger)
+        {
+            flurlClient = flurlFac.Get("https://cinnamon.ph/blog/wp-json/wp/v2/");
+            _logger = logger;
+        }
+
+        public async Task<AppResult<List<Post>>> GetPostsAsync()
         {
             try
             {
-                var posts = await $"{WordPressAPI}/posts"
-                    .GetJsonAsync<List<WordPressPost>>();
-                return posts;
+                var result = await flurlClient.Request("/posts").GetJsonAsync<List<Post>>();
+                return AppResult<List<Post>>.CreateSucceeded(result, "Wordpress API connection is successful");
             }
             catch (FlurlHttpException ex)
             {
-                // Handle HTTP errors here (e.g., log them)
-                throw;
+                var error = await ex.GetResponseJsonAsync();
+                return AppResult<List<Post>>.CreateFailed(ex, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return AppResult<List<Post>>.CreateFailed(ex, "An error occurred when getting WordPress API");
+            }
+        }
+
+        public async Task<AppResult<MediaPost>> GetMediaAsync(int mediaId)
+        {
+            try
+            {
+                var result = await flurlClient.Request($"/media/{mediaId}").GetJsonAsync<MediaPost>();
+                return AppResult<MediaPost>.CreateSucceeded(result, "Wordpress MediaPost API connection is successful");
+            }
+            catch (FlurlHttpException ex)
+            {
+                var error = await ex.GetResponseJsonAsync();
+                return AppResult<MediaPost>.CreateFailed(ex, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return AppResult<MediaPost>.CreateFailed(ex, "An error occurred when getting WordPress MediaPost API");
             }
         }
     }
-}
