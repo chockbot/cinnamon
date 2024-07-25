6fa095f6168e99c03b7b65b12a4cd00cb1df1053
dechalico <dechalico.dev@gmail.com>
2024-07-25 17:13:17 +0800

    ote create request payment handler

---
+++ src/api/Cinnamon.Api.Core/Services/Transaction/Interactors/OteCreateRequestPaymentArgs.cs
@@ -0,0 +1,28 @@
+using Cinnamon.Framework.Interactor;
+
+namespace Cinnamon.Api.Core.Services.TransactionService.Interactors;
+
+public class OteCreateRequestPaymentArgs : IInteractor
+{
+    public int CustomerId {get; set;}
+    public int ActivityId {get; set;}
+    public DateTime SelectedDate {get; set;}
+    public IEnumerable<RequestPaymentTicket> SelectedTickets {get; set;} = Enumerable.Empty<RequestPaymentTicket>();
+    public string? Guid {get; set;}
+    public string? Token {get; set;}
+
+    public IEnumerable<ProviderQuestion>? Questions {get; set;}
+
+    public class RequestPaymentTicket 
+    {
+        public int TicketId {get; set;}
+        public int TicketCount {get; set;}
+    }
+
+    public class ProviderQuestion 
+    {
+        public int Id {get; set;}
+        public string Question {get; set;}
+        public string? Answer {get; set;}
+    }
+}
