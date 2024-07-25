07a8cad8a4ecfe0cda5faf8c5424183107bde73f
Rica Namoro <rica.namoro@cinnamon.ph>
2024-07-25 16:00:27 +0800

    js update

--- src/Cinnamon.Web/JsLib/src/utils/hoverEffect.js
+++ src/Cinnamon.Web/JsLib/src/utils/hoverEffect.js
@@ -1,6 +1,6 @@
-const hoverEffect = {
-    moveEffect: async function (event) {
-        const button = event.currentTarget;
+export async function moveEffect(event, inputSelector) {
+    debugger;
+        const button = document.querySelector(inputSelector);
         const effect = button.querySelector('.effect');
         const rect = button.getBoundingClientRect();
         const x = event.clientX - rect.left;
@@ -10,14 +10,17 @@ const hoverEffect = {
         effect.style.top = `${y}px`;
         effect.style.transform = 'translate(-50%, -50%) scale(1)';
         effect.style.opacity = '1';
-    },
+}
 
-    hideEffect: async function (event) {
-        const button = event.currentTarget;
+export async function hideEffect(inputSelector) {
+    debugger;
+        const button = document.querySelector(inputSelector);
         const effect = button.querySelector('.effect');
         effect.style.transform = 'translate(-50%, -50%) scale(0)';
         effect.style.opacity = '0';
-    }
-};
+}
 
-window.hoverEffect = hoverEffect;
+export default {
+    moveEffect,
+    hideEffect
+};
