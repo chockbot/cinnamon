294ccedb5b74099d4868a404276e111e63992b8b
TartPops6 <pikuraonmyoji@gmail.com>
2024-07-26 09:15:17 +0800

    added hover and transition ui on my events section of dashboard

--- src/Cinnamon.Web/JsLib/src/utils/hoverEffect.js
+++ src/Cinnamon.Web/JsLib/src/utils/hoverEffect.js
@@ -1,26 +1,24 @@
 export async function moveEffect(event, inputSelector) {
-    debugger;
-        const button = document.querySelector(inputSelector);
-        const effect = button.querySelector('.effect');
-        const rect = button.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
+    const button = document.querySelector(inputSelector);
+    const effect = button.querySelector('.effect');
+    const rect = button.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
 
-        effect.style.left = `${x}px`;
-        effect.style.top = `${y}px`;
-        effect.style.transform = 'translate(-50%, -50%) scale(1)';
-        effect.style.opacity = '1';
+    effect.style.left = `${x}px`;
+    effect.style.top = `${y}px`;
+    effect.style.transform = 'translate(-50%, -50%) scale(1)';
+    effect.style.opacity = '1';
 }
 
 export async function hideEffect(inputSelector) {
-    debugger;
-        const button = document.querySelector(inputSelector);
-        const effect = button.querySelector('.effect');
-        effect.style.transform = 'translate(-50%, -50%) scale(0)';
-        effect.style.opacity = '0';
+    const button = document.querySelector(inputSelector);
+    const effect = button.querySelector('.effect');
+    effect.style.transform = 'translate(-50%, -50%) scale(0)';
+    effect.style.opacity = '0';
 }
 
 export default {
-    moveEffect,
-    hideEffect
+moveEffect,
+hideEffect
 };
