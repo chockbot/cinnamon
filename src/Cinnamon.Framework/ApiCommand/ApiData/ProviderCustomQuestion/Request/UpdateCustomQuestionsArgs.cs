b6dad406a2670224dad309878c9476f8d83346b2
dechalico <dechalico.dev@gmail.com>
2024-08-01 00:16:27 +0800

    api data endpoint for update and delete provider questions

---
+++ src/Cinnamon.Framework/ApiCommand/ApiData/ProviderCustomQuestion/Request/UpdateCustomQuestionsArgs.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Cinnamon.Framework.ApiCommand.ApiData.ProviderCustomQuestion.Request;
+
+public class UpdateCustomQuestionsArgs 
+{
+    [Required]
+    public IEnumerable<UpdateCustomQuestions> Questions {get; set;}
+    
+    public class UpdateCustomQuestions
+    {
+        [Required]
+        public int Id {get; set;}
+
+        [Required]
+        public int ActivityId {get; set;}
+
+        [Required]
+        public int ProviderId {get; set;}
+
+        [Required]
+        public string FieldLabel {get; set;}
+
+        [Required]
+        public string FieldType {get; set;}
+
+        [Required]
+        public bool Required {get; set;}
+    }
+}
