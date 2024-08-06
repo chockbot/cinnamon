a0b8caed2fb1417c5b47c114aa196ba0115874ba
Rica Namoro <rica.namoro@cinnamon.ph>
2024-08-07 07:32:38 +0800

    fix cover photo bug

--- src/Cinnamon.Web/Modules/Services/GoogleDriveServices.cs
+++ src/Cinnamon.Web/Modules/Services/GoogleDriveServices.cs
@@ -52,7 +52,7 @@ public class GoogleDriveService
             Id = file.Id,
             Name = file.Name,
             Type = file.MimeType,
-            ThumbnailLink = file.ImageMediaMetadata.ToString(),
+            ThumbnailLink = file.ThumbnailLink,
             ModifiedDate = file.ModifiedTime // Add modified time property
         }).OrderByDescending(file => file.ModifiedDate).ToList();
 
