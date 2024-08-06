29274dd1d99ebb44011fd6e9bb5f8a7f1883d9cd
dechalico <dechalico.dev@gmail.com>
2024-08-06 14:01:58 +0800

    update chat room name handler

--- src/api/Cinnamon.Api.Core/Services/ChatService/UpdateChatRoomNameHandler.cs
+++ src/api/Cinnamon.Api.Core/Services/ChatService/UpdateChatRoomNameHandler.cs
@@ -1,3 +1,4 @@
+using Cinnamon.Api.Core.Modules.DataAccess.Handlers;
 using Cinnamon.Api.Core.Services.AccountService.Handlers;
 using Cinnamon.Api.Core.Services.ChatService.Handlers;
 using Cinnamon.Api.Core.Services.ChatService.Interactors;
@@ -9,6 +10,7 @@ namespace Cinnamon.Api.Core.Services.ChatService;
 public class UpdateChatRoomNameHandler : IUpdateChatRoomNameHandler
 {
     private readonly IGetProfileHandler getProfileHandler;
+    private readonly IChatRoomData chatRoomData;
 
     public UpdateChatRoomNameHandler(IGetProfileHandler getProfileHandler)
     {
@@ -29,6 +31,43 @@ public class UpdateChatRoomNameHandler : IUpdateChatRoomNameHandler
             {
                 return AppResult<UpdateChatRoomNameResult>.CreateFailed(new ApplicationException(currentProfile.Message),"Failed to get current profile");
             }
+            var profile = currentProfile.Result;
+
+            var chatMembers = await chatRoomData.GetChatRoomMembers(new Framework.ApiCommand.ApiData.ChatConnection.Request.GetChatMemberArgs {
+                ChatRoomId = args.ChatRoomId,
+                UserId = profile.Id
+            });
+            if(!chatMembers.Succeeded || chatMembers.Result is null || !chatMembers.Result.IsSuccess)
+            {
+                return AppResult<UpdateChatRoomNameResult>.CreateFailed(new ApplicationException(chatMembers.Message),"Failed to get chat members");
+            }
+
+            if(chatMembers.Result.Result.Count() == 0)
+            {
+                return AppResult<UpdateChatRoomNameResult>.CreateFailed(
+                    new ApplicationException("User is not a member of the chat room"),"User is not a member of the chat room");
+            }
+
+            bool isOwner = chatMembers.Result.Result.Any(c => c.ChatMemberType == 0);
+
+            if(!isOwner)
+            {
+                return AppResult<UpdateChatRoomNameResult>.CreateFailed(
+                    new ApplicationException("User is not the owner of the chat room"),"User is not the owner of the chat room");
+            }
+
+            var updateChatRoomNameRes = await chatRoomData.UpdateChatRoomName(new Framework.ApiCommand.ApiData.ChatConnection.Request.UpdateChatRoomNameArgs {
+                ChatRoomId = args.ChatRoomId,
+                NewChatRoomName = args.ChatRoomName
+            });
+            if(!updateChatRoomNameRes.Succeeded || updateChatRoomNameRes.Result is null || !updateChatRoomNameRes.Result.IsSuccess)
+            {
+                return AppResult<UpdateChatRoomNameResult>.CreateFailed(new ApplicationException(updateChatRoomNameRes.Message),"Failed to update chat room name");
+            }
+
+            return AppResult<UpdateChatRoomNameResult>.CreateSucceeded(new UpdateChatRoomNameResult {
+                IsSuccess = true
+            }, "Chat room name updated successfully");
         }
         catch (Exception ex)
         {
