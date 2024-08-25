7112f56f42dcf0da4ed6942f24d5c158d6439b89
dechalico <dechalico.dev@gmail.com>
2024-08-26 00:59:32 +0800

    get seat plan templates

--- src/Cinnamon.Framework/ApiCommand/ApiCore/DTO/SeatPlan/SeatPlanTemplateDTO.cs
+++ src/Cinnamon.Framework/ApiCommand/ApiCore/DTO/SeatPlan/SeatPlanTemplateDTO.cs
@@ -3,6 +3,7 @@ namespace Cinnamon.Framework.ApiCommand.ApiCore.DTO.SeatPlan;
 public class SeatPlanTemplateDTO
 {
     public string Id { get; set; }
+    public int SeatPlanFormatterId { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
     public string ImageSrc { get; set; }
