c8470e70c49fbe52768c3bb4998fe51fc5a1d000
dechalico <dechalico.dev@gmail.com>
2024-08-26 22:03:42 +0800

    used dictionary instead of list

--- src/Cinnamon.Framework/Models/SeatPlan/Format.cs
+++ src/Cinnamon.Framework/Models/SeatPlan/Format.cs
@@ -3,26 +3,32 @@ namespace Cinnamon.Framework.Models.SeatPlan;
 public class Format 
 {
     public string Name { get; set; }
-    public IList<Category> Categories { get; set; }
+    public IDictionary<string, Category> Categories { get; set; }
 }
 
 public class Category 
 {
+    public string Uuid { get; set; }
     public string Name { get; set; }
     public string Color { get; set; }
-    public IList<Row> Rows { get; set; }
+    public IDictionary<string, Row> Rows { get; set; }
 }
 
 public class Row 
 {
+    public string Uuid { get; set; }
     public string Color { get; set; }
     public string Category { get; set; }
     public string RowNumber { get; set; }
-    public IList<Seat> Seats { get; set; }
+    public IDictionary<string, Seat> Seats { get; set; }
 }
 
 public class Seat 
 {
     public string SeatNumber { get; set; }
     public string Uuid { get; set; }
+
+    // for extra fields need for creation and purchase
+    public bool Occupied { get; set; }
+    public bool Excluded { get; set; }
 }
