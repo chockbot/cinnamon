74db699958a433723cee68a30240b3c5a99dd619
dechalico <dechalico.dev@gmail.com>
2024-09-05 10:29:29 +0800

    api core reserve seats mapped uuid

--- src/api/Cinnamon.Api.Core/Controllers/ActivityController.cs
+++ src/api/Cinnamon.Api.Core/Controllers/ActivityController.cs
@@ -2683,7 +2683,8 @@ public class ActivityController : ControllerBase
                         Price = p.Price,
                         Name = p.Name,
                         IsUnlimited = p.IsUnlimited,
-                        RequiredApproval = p.RequiredApproval
+                        RequiredApproval = p.RequiredApproval,
+                        ReserveSeatUuid = p.ReserveSeatUuid
                     };
                 }),
                 DateOverrides = args.DateOverrides is not null ? 
