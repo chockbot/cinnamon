5544dd2c2e2960726444b1d3dcdea4a6eef17f85
dechalico <dechalico.dev@gmail.com>
2024-09-05 10:07:53 +0800

    add reserve seat uuid

---
+++ src/api/Cinnamon.Api.Data/Migrations/20240905020606_SchedulePricingsAddReserveSeatUUid.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinnamon.Api.Data.Migrations
+{
+    public partial class SchedulePricingsAddReserveSeatUUid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReserveSeatUuid",
+                table: "OteSchedulePricings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReserveSeatUuid",
+                table: "OteSchedulePricingGroups",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReserveSeatUuid",
+                table: "OteSchedulePricings");
+
+            migrationBuilder.DropColumn(
+                name: "ReserveSeatUuid",
+                table: "OteSchedulePricingGroups");
+        }
+    }
+}
