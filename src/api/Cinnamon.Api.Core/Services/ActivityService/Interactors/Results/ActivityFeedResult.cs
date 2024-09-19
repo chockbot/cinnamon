7e835d3819ce886fac3b54eb390ff36da8afd172
TartPops6 <pikuraonmyoji@gmail.com>
2024-09-19 13:10:21 +0800

    made the dates on explore page dynamic

--- src/api/Cinnamon.Api.Core/Services/ActivityService/Interactors/Results/ActivityFeedResult.cs
+++ src/api/Cinnamon.Api.Core/Services/ActivityService/Interactors/Results/ActivityFeedResult.cs
@@ -15,6 +15,9 @@ public class ActivityFeedResult
         public bool IsNew {get; set;}
         public DateTime To { get; set; }
         public DateTime From { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
         public string StartTime { get; set; }
         public Location Address {get; set;}
         public Summary SummaryDetails {get; set;}
