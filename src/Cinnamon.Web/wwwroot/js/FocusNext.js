2d53eb61b302520e9be32b2ab22e2788ed41f3f1
dvrica07 <namoro.rica@outlook.com>
2024-12-03 10:55:22 +0800

    fix js for continues typing otp code

--- src/Cinnamon.Web/wwwroot/js/FocusNext.js
+++ src/Cinnamon.Web/wwwroot/js/FocusNext.js
@@ -1,6 +1,12 @@
 ﻿export function focusNextOtpInput(currentIndex) {
     var nextElement = document.querySelector(`#otp-${currentIndex}`);
     if (nextElement) {
-        nextElement.focus();
+        try {
+            nextElement.focus({ preventScroll: true }); // preventScroll ensures the input stays visible
+        } catch (e) {
+            console.error("Focus failed:", e);
+        }
+    } else {
+        console.warn("Element not found for index:", currentIndex);
     }
 }
