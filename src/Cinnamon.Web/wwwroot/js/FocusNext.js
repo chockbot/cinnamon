04df68b83ab6e253383261f1aed3c0f502f97261
dvrica07 <namoro.rica@outlook.com>
2024-12-03 11:07:08 +0800

    update

--- src/Cinnamon.Web/wwwroot/js/FocusNext.js
+++ src/Cinnamon.Web/wwwroot/js/FocusNext.js
@@ -1,12 +1,6 @@
 ﻿export function focusNextOtpInput(currentIndex) {
     var nextElement = document.querySelector(`#otp-${currentIndex}`);
     if (nextElement) {
-        try {
-            nextElement.focus({ preventScroll: true }); // preventScroll ensures the input stays visible
-        } catch (e) {
-            console.error("Focus failed:", e);
-        }
-    } else {
-        console.warn("Element not found for index:", currentIndex);
+        nextElement.focus();
     }
 }
